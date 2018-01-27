e4deae6784b6c006dab5511673dfed93ceaf98c8
Tony Narlock <tony@git-pull.com>
2018-01-27 16:40:57 -0600

    initial example data and template for patch

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -39,4 +39,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.item {
+  background-color: #fff;
+  box-shadow: 0 2px 3px hsla(0,0%,4%,.1), 0 0 0 1px hsla(0,0%,4%,.1);
+  color: #4a4a4a;
+  max-width: 100%;
+  position: relative;
+}
 </style>
--- vue/src/components/item_types/Patch.vue
+++ vue/src/components/item_types/Patch.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>must be static tpl!</div>
+  <div class="item">
+    Patch:
+    <h1>{{item.title}}</h1>
+    <a :href="item.in_re_url" target="_blank">issue</a>
+    <a :href="item.qa_url" target="_blank">qa</a>
+    <a :href="item.repo_url" target="_blank">repo</a>
+    <a :href="item.diff_url" target="_blank">patch</a>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Patch',
-  props: ['opts'],
+  props: ['opts', 'item'],
   computed: {
     log: () => JSON.stringify(this.opts),
   },
