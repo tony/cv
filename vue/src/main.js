ec562f034a24dd662fa1c20832b7cc7cd7b3f767
Tony Narlock <tony@git-pull.com>
2018-01-28 10:41:30 -0600

    rm normalizr from code

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,6 @@
 // https://github.com/vuejs/vue/issues/3270#issuecomment-232269588
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { schema } from 'normalizr';
 import App from './App';
 import router from './router';
 import items from './cv.json';
@@ -14,17 +13,29 @@ Vue.config.productionTip = false;
 Vue.use(Vuex);
 
 // subject
-export const project = new schema.Entity('projects');
-export const company = new schema.Entity('companies');
-export const publication = new schema.Entity('publications');
-export const club = new schema.Entity('clubs');
+// project, 'projects'
+// company, 'companies'
+// publication, 'publications'
+// club, 'clubs'
 
+// verb / actions
+// kudo, 'kudos'; // hn/reddit/blog/tweet/review
+// patch, 'patches' //  gh, bugzilla
+// status, 'status // role
+// achievement, 'achievements  // exit, cert
+// event, 'achievements' // released, started
+// presentation = 'presentations'
+
+// adjective
+// language, 'languages'
+// role, 'roles' // employee, volunteere, president
 
 function validateProject(item) {
   console.assert('name' in item, item, 'name not in item');
   console.assert('url' in item, item, 'url not in item');
   console.assert('repo_url' in item, item, 'repo_url not in item');
 }
+
 function validateSubjects(list) {
   list.forEach((item) => {
     switch (item.subject) {
@@ -37,20 +48,6 @@ function validateSubjects(list) {
   });
 }
 validateSubjects(subjects);
-
-// verb / actions
-export const kudo = new schema.Entity('kudos'); // hn/reddit/blog/tweet/review
-export const patch = new schema.Entity('patches', {
-  project,
-}); // gh, bugzilla
-export const status = new schema.Entity('status'); // role
-export const achievement = new schema.Entity('achievements'); // exit, cert
-export const event = new schema.Entity('achievements'); // released, started
-export const presentation = new schema.Entity('presentations');
-
-// adjective
-export const language = new schema.Entity('languages');
-export const role = new schema.Entity('roles'); // employee, volunteere, president
 
 function normalizeVerbs(verbs) {
   verbs.map((item) => {
