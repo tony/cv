29540f8c9d0a5c8a8b910888bc2890f44067f8e4
Tony Narlock <tony@git-pull.com>
2018-01-28 07:45:26 -0600

    add base normalizr schemas

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,6 +3,7 @@
 // https://github.com/vuejs/vue/issues/3270#issuecomment-232269588
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { schema } from 'normalizr';
 import App from './App';
 import router from './router';
 import items from './cv.json';
@@ -10,6 +11,24 @@ import items from './cv.json';
 Vue.config.productionTip = false;
 
 Vue.use(Vuex);
+
+// subject
+export const project = new schema.Entity('projects');
+export const company = new schema.Entity('companies');
+export const publication = new schema.Entity('publications');
+export const club = new schema.Entity('clubs');
+
+// verb
+export const kudo = new schema.Entity('kudos'); // hn/reddit/blog/tweet/review
+export const patch = new schema.Entity('patches'); // gh, bugzilla
+export const status = new schema.Entity('status'); // role
+export const achievement = new schema.Entity('achievements'); // exit, cert
+export const event = new schema.Entity('achievements'); // released, started
+export const presentation = new schema.Entity('presentations');
+
+// adjective
+export const language = new schema.Entity('languages');
+export const role = new schema.Entity('roles'); // employee, volunteere, president
 
 const store = new Vuex.Store({
   state: {
