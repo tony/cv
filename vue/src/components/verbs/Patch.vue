92c0871bb01763b01adb4e47f3748fb44f14d516
Tony Narlock <tony@git-pull.com>
2018-01-28 14:55:28 -0600

    initial layout for patch

--- vue/src/components/verbs/Patch.vue
+++ vue/src/components/verbs/Patch.vue
@@ -1,20 +1,30 @@
 <template>
-  <div class="item">
-    Patch:
-    <h1>{{item.title}}</h1>
-    <a :href="item.in_re_url" target="_blank">issue</a>
-    <a :href="item.qa_url" target="_blank">qa</a>
-    <a :href="item.repo_url" target="_blank">repo</a>
-    <a :href="item.diff_url" target="_blank">patch</a>
-    <ul>
-      <li v-for="language in item.languages" :key="language.id">
+  <div class="row">
+    <div class="col-xs-10 col-xs-offset-1">
+    <div class="box">
+    <div class="row">
+      <div class="col-xs-4 item"><div class="box">
+      <h1>Patch</h1>
+      <p>
+      <span class="tag" v-for="language in item.languages" :key="language.id">
       {{language}}
-      </li>
-    </ul>
-    {{item.project.name}}
-    {{item.project}}
-    {{item.project.url}}
-    {{item.project.repo_url}}
+      </span>
+      </p>
+      </div></div>
+      <div class="col-xs-8 item"><div class="box">
+        <h2>{{item.title}}</h2>
+        <em>for
+        <a :href="item.project.url" target="_blank">{{item.project.name}}</a>
+        </em>
+        <small>(<a :href="item.project.repo_url" target="_blank">repo</a>)</small>
+        <br />
+        (<a :href="item.in_re_url" target="_blank">issue</a>,
+        <a :href="item.qa_url" target="_blank">qa</a>,
+        <a :href="item.diff_url" target="_blank">patch</a>)
+      </div></div>
+    </div>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -29,4 +39,19 @@ export default {
 </script>
 
 <style>
+.tag {
+  align-items: center;
+  background-color: #f5f5f5;
+  border-radius: 3px;
+  color: #4a4a4a;
+  display: inline-flex;
+  font-size: .75rem;
+  justify-content: center;
+  line-height: 1.5;
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+  padding-left: .75em;
+  padding-right: .75em;
+  white-space: nowrap;
+}
 </style>
