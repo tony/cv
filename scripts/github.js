eb92916b41d45461c828eeece6311fbdae7e0855
Tony Narlock <tony@git-pull.com>
2018-01-30 11:37:03 -0600

    use ISO_8601 formatted dates

--- scripts/github.js
+++ scripts/github.js
@@ -147,8 +147,8 @@ recursePRQuery(initialPrQuery).then(prs => {
       component: 'Patch',
       qa_url: pr.url,
       diff_url: pr.url + '.diff',
-      proposed_date: moment(pr.createdAt).format('YYYY-MM-D'),
-      accepted_date: pr.mergedAt ? moment(pr.mergedAt).format('YYYY-MM-D') : undefined,
+      proposed_date: moment(pr.createdAt).format('YYYY-MM-DD'),
+      accepted_date: pr.mergedAt ? moment(pr.mergedAt).format('YYYY-MM-DD') : undefined,
       title: pr.title,
       project: projects_final.find(p => pr.repository.name == p.name).id,
     }
