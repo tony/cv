49045150b62fca41e6fce71087b6bae87b87bb01
Tony Narlock <tony@git-pull.com>
2018-02-02 11:39:44 -0600

    improve ignore patterns

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -36,13 +36,13 @@ function availableSubjects(subjects, availableActivities) {
 }
 
 
-const filterTypos = v => v.title.match(/(typo|Typo|spelling|Spelling|note|Note)/);
+const filterTypos = v => v.title.match(/(typo|Typo|spelling|Spelling|note|Note|correct|Correct|Fix type|Fix URL)/);
 const filterIgnoreTypos = v => !filterTypos(v);
 
-const filterDocs = v => v.title.match(/(doc|Doc|license|LICENSE|README|readme|link|Link|\.md|instructions|Instructions|guidelines|pypi badge)/);
+const filterDocs = v => v.title.match(/(doc|Doc|license|LICENSE|README|readme|link|Link|\.md|instructions|Instructions|guidelines|pypi badge|AUTHORS|License|changelog|label|copyright|add cookiecutter|issue template)/);
 const filterIgnoreDocs = v => !filterDocs(v);
 
-const filterCodeStyle = v => v.title.match(/(indent|Indent|whitespace|spacing|lint|Lint|sort|Sort|jshint|PEP|pep8|tabs|Tabs)/);
+const filterCodeStyle = v => v.title.match(/(indent|Indent|whitespace|spacing|lint|Lint|sort|Sort|jshint|PEP|pep8|tabs|Tabs|Ignore|ignore|__about__|import|tweak|Tweak|hash|modernize|Add.*module|trivial|travis|Travis|dependency|MANIFEST.in)/);
 const filterIgnoreCodeStyle = v => !filterCodeStyle(v);
 
 const filterUnmerged = v => ('accepted_date' in v ? v.accepted_date : true);
