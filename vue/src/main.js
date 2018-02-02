e149f21ff51fc6549613f830259158d584f03347
Tony Narlock <tony@git-pull.com>
2018-02-02 09:47:18 -0600

    split octicon loading into separate module

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,34 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // https://github.com/vuejs/vue/issues/3270#issuecomment-232269588
 import Vue from 'vue';
-import Octicon from 'vue-octicon/components/Octicon';
-import 'vue-octicon/icons/diff';
-import 'vue-octicon/icons/mark-github';
-import 'vue-octicon/icons/git-merge';
-import 'vue-octicon/icons/check';
-import 'vue-octicon/icons/repo';
-import 'vue-octicon/icons/book';
-import 'vue-octicon/icons/code';
-import 'vue-octicon/icons/home';
-import 'vue-octicon/icons/dashboard';
-import 'vue-octicon/icons/list-unordered';
-import 'vue-octicon/icons/graph';
-import 'vue-octicon/icons/search';
-import 'vue-octicon/icons/checklist';
-import 'vue-octicon/icons/comment-discussion';
-import 'vue-octicon/icons/git-pull-request';
-
 import App from './App';
 import router from './router';
 import store from './store';
 import { LOAD_INITIAL_DATA } from './store/mutation-types';
 import { ACTIVITIES, SUBJECTS, DEFAULT_SELECTED_FILTERS } from './constants';
+import './lib/octicons';
 
 Vue.config.productionTip = false;
 
 Vue.use(require('vue-moment'));
-
-Vue.component('octicon', Octicon);
 
 // Load initial data
 store.commit(
