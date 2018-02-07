dbd3f4c46c647587ccc49472bd487232c18f9c21
Tony Narlock <tony@git-pull.com>
2018-02-07 08:08:40 -0600

    split out multiselect into Lenses component

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -9,111 +9,25 @@
     <a href="https://www.git-pull.com/mentoring/" target="_blank">mentoring</a>.</em></p>
 
     <Charts />
-    <div class="row"><div class="col-xs-10 col-xs-offset-1">
-      <multiselect
-        :search="true"
-        :multiple="true"
-        placeholder="Lookup by Place/Project/Company - e.g. tmuxp, Social Amp, The Tao of tmux"
-        label="name"
-        track-by="name"
-        id="subjectFilter"
-        :value="selectedSubjects"
-        :options="availableSubjects"
-        @input="updateSelectedSubjectsAction"
-        >
-        <template slot="option" slot-scope="props">
-        <span>{{ props.option.name }}</span>
-        <span class="tag">
-        {{props.option.type}}
-        </span>
-        </template>
-      </multiselect>
-
-      <multiselect
-        :search="true"
-        :multiple="true"
-        placeholder="Filter by Activity Type(s) - e.g. Work, Open Source, Website, Volunteering"
-        id="activityFilter"
-        track-by="name"
-        :options="availableActivityTypes"
-        :value="selectedActivityTypes"
-        @input="updateSelectedActivityTypeAction"
-        label="name"
-       >
-      </multiselect>
-
-      <multiselect
-        :search="true"
-        :multiple="true"
-        placeholder="Filter by Programming Language(s) - e.g. Python, JavaScript, C++"
-        id="languageFilter"
-        :options="availableLanguages"
-        :value="selectedLanguages"
-        track-by="name"
-        label="name"
-        @input="updateSelectedLanguagesAction"
-       >
-      </multiselect>
-
-      <div class="row choices">
-        <div v-for="fil in availableFilters" :key="fil.id" class="roundedOne col-sm">
-          <div class="box">
-            <input type="checkbox" :id="fil" :value="fil" v-model="selectedFilters" >
-            <label :for="fil">{{fil}}</label>
-          </div>
-        </div>
-      </div>
-    </div></div>
+    <Lenses />
   </div>
 </template>
 
 <script>
-import Multiselect from 'vue-multiselect';
-import { mapGetters, mapActions, mapState } from 'vuex';
 import Charts from './Charts';
+import Lenses from './Lenses';
 
 export default {
   name: 'Header',
-  components: { Multiselect, Charts },
+  components: { Charts, Lenses },
   data() {
     return {
       msg: 'Tony Narlock\'s CV',
     };
   },
-  computed: {
-    ...mapGetters([
-      'subjects',
-      'availableSubjects',
-      'availableSubjectTypes',
-      'availableActivityTypes',
-      'availableFilters',
-      'availableLanguages',
-      'filteredActivitiesMinusProjects',
-    ]),
-    ...{
-      selectedFilters: {
-        get() {
-          return this.$store.state.selectedFilters;
-        },
-        set(value) {
-          this.$store.commit('updateSelectedFilters', value);
-        },
-      },
-    },
-    ...mapState(['selectedActivityTypes', 'selectedSubjects', 'selectedLanguages']),
-  },
-  methods: {
-    ...mapActions([
-      'updateSelectedActivityTypeAction',
-      'updateSelectedSubjectsAction',
-      'updateSelectedFiltersAction',
-      'updateSelectedLanguagesAction',
-    ]),
-  },
 };
 </script>
 
-<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 .header h1, .header h2 {
