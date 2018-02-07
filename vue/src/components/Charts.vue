899b0fd62bf7c49b672c60882e11a7aefc3c96e8
Tony Narlock <tony@git-pull.com>
2018-02-07 08:02:16 -0600

    split Charts into its own component

---
+++ vue/src/components/Charts.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="charts row padBottom">
+    <div class="col-sm-2 col-sm-offset-2">
+      <language-pie
+        :chart-data="languages"
+        :height="250"
+        />
+    </div>
+    <div class="col-sm-6 ">
+      <activity-line
+        :chart-data="activityTimes"
+        :height="250"
+        />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+import LanguagePie from './charts/LanguagePie';
+import ActivityLine from './charts/ActivityLine';
+
+export default {
+  name: 'Charts',
+  components: { LanguagePie, ActivityLine },
+  computed: {
+    ...mapGetters(['filteredActivitiesMinusProjects']),
+    ...{
+      languages() {
+        // https://github.com/airbnb/javascript/issues/719
+        let l = this.filteredActivitiesMinusProjects.reduce((languages, activity) => {
+          const rLanguages = languages;
+          if (activity.project.languages && activity.project.languages.length) {
+            activity.project.languages.forEach((lang) => {
+              if (lang.name in rLanguages) {
+                rLanguages[lang.name].count += 1;
+              } else {
+                rLanguages[lang.name] = {
+                  count: 1,
+                  color: lang.color,
+                  name: lang.name,
+                };
+              }
+            });
+          }
+          return rLanguages;
+        }, {});
+
+        // flatten
+        l = Object.keys(l).map(key => l[key]);
+        return {
+          labels: l.map(lang => lang.name),
+          datasets: [{
+            backgroundColor: l.map(lang => lang.color),
+            data: l.map(lang => lang.count),
+          }],
+        };
+      },
+      activityTimes() {
+        // https://github.com/airbnb/javascript/issues/719
+        let l = this.filteredActivitiesMinusProjects.reduce((acc1, activity) => {
+          const acc = acc1;
+          if (activity.created_date) {
+            const year = Vue.moment(activity.created_date).get('year');
+            if (year in acc) {
+              acc[year].count += 1;
+            } else {
+              acc[year] = {
+                count: 1,
+                name: year,
+              };
+            }
+          }
+          return acc;
+        }, {});
+
+        // flatten
+        l = Object.keys(l).map(key => l[key]);
+        return {
+          labels: l.map(lang => lang.name),
+          datasets: [
+            { data: l.map(lang => lang.count) },
+          ],
+        };
+      },
+    },
+  },
+};
+</script>
+
+
+<style>
+.padBottom {
+  margin-bottom: 1em;
+}
+</style>
--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -8,21 +8,7 @@
       I'm also available for <a href="https://www.git-pull.com/consulting/" target="_blank">consulting</a> and
     <a href="https://www.git-pull.com/mentoring/" target="_blank">mentoring</a>.</em></p>
 
-    <div class="row padBottom">
-      <div class="col-sm-2 col-sm-offset-2">
-        <language-pie
-          :chart-data="languages"
-          :height="250"
-          />
-      </div>
-      <div class="col-sm-6 ">
-        <activity-line
-          :chart-data="activityTimes"
-          :height="250"
-          />
-      </div>
-    </div>
-
+    <Charts />
     <div class="row"><div class="col-xs-10 col-xs-offset-1">
       <multiselect
         :search="true"
@@ -83,14 +69,12 @@
 
 <script>
 import Multiselect from 'vue-multiselect';
-import Vue from 'vue';
 import { mapGetters, mapActions, mapState } from 'vuex';
-import LanguagePie from './charts/LanguagePie';
-import ActivityLine from './charts/ActivityLine';
+import Charts from './Charts';
 
 export default {
   name: 'Header',
-  components: { Multiselect, LanguagePie, ActivityLine },
+  components: { Multiselect, Charts },
   data() {
     return {
       msg: 'Tony Narlock\'s CV',
@@ -117,65 +101,6 @@ export default {
       },
     },
     ...mapState(['selectedActivityTypes', 'selectedSubjects', 'selectedLanguages']),
-    ...{
-      languages() {
-        // https://github.com/airbnb/javascript/issues/719
-        let l = this.filteredActivitiesMinusProjects.reduce((languages, activity) => {
-          const rLanguages = languages;
-          if (activity.project.languages && activity.project.languages.length) {
-            activity.project.languages.forEach((lang) => {
-              if (lang.name in rLanguages) {
-                rLanguages[lang.name].count += 1;
-              } else {
-                rLanguages[lang.name] = {
-                  count: 1,
-                  color: lang.color,
-                  name: lang.name,
-                };
-              }
-            });
-          }
-          return rLanguages;
-        }, {});
-
-        // flatten
-        l = Object.keys(l).map(key => l[key]);
-        return {
-          labels: l.map(lang => lang.name),
-          datasets: [{
-            backgroundColor: l.map(lang => lang.color),
-            data: l.map(lang => lang.count),
-          }],
-        };
-      },
-      activityTimes() {
-        // https://github.com/airbnb/javascript/issues/719
-        let l = this.filteredActivitiesMinusProjects.reduce((acc1, activity) => {
-          const acc = acc1;
-          if (activity.created_date) {
-            const year = Vue.moment(activity.created_date).get('year');
-            if (year in acc) {
-              acc[year].count += 1;
-            } else {
-              acc[year] = {
-                count: 1,
-                name: year,
-              };
-            }
-          }
-          return acc;
-        }, {});
-
-        // flatten
-        l = Object.keys(l).map(key => l[key]);
-        return {
-          labels: l.map(lang => lang.name),
-          datasets: [
-            { data: l.map(lang => lang.count) },
-          ],
-        };
-      },
-    },
   },
   methods: {
     ...mapActions([
@@ -263,8 +188,5 @@ export default {
 }
 .hidden {
   display: none;
-}
-.padBottom {
-  margin-bottom: 1em;
 }
 </style>
