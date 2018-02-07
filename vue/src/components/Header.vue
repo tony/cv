150ee7f45c03222eb6628a0d3650bc5956e5fb87
Tony Narlock <tony@git-pull.com>
2018-02-07 11:55:36 -0600

    update copy in header

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <h1>{{ msg }}</h1>
 
-    <p><em>Friendly reminder:
+    <p><em>Friendly note:
       I don't do technical screens<a href="https://www.git-pull.com/blog/2018/01/technical-screening.html" target="_blank" class="hidden footnote">[1]</a>.
       Seeking remote, senior programming roles.
       I'm also available for <a href="https://www.git-pull.com/consulting/" target="_blank">consulting</a> and
