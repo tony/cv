c6e825eaf0c0d71774501150cce82a07a5b753c4
Tony Narlock <tony@git-pull.com>
2018-02-08 04:50:12 -0600

    Checkpoint: todo list
    From https://redux.js.org/docs/basics/ExampleTodoList.html
    
    2018-02-08

--- react/src/index.js
+++ react/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(todoApp)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
