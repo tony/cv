3972dd92a39b506f412df3b6a8d0f502a164ee1c
Tony Narlock <tony@git-pull.com>
2018-02-08 05:00:27 -0600

    add initial state data

--- react/src/index.js
+++ react/src/index.js
@@ -6,8 +6,11 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import { addTodo } from './actions';
 
 let store = createStore(todoApp)
+
+store.dispatch(addTodo('hi'));
 
 ReactDOM.render(
   <Provider store={store}>
