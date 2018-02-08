29741033255fd0bce19968f193ad74eee8137934
Tony Narlock <tony@git-pull.com>
2018-02-08 13:51:14 -0600

    get css from vue working on react, add TODO to IDEAS for DOM parity

--- react/src/index.js
+++ react/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'cv-lib/assets/css/flexboxgrid.min.css';
 import 'cv-lib/assets/css/git-pull.css';
+import 'cv-lib/assets/css/cv.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
