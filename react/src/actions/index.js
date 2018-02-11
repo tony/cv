9e33a6df727c28d7bbf35f0f324f8fa5f11bd988
Tony Narlock <tony@git-pull.com>
2018-02-11 15:32:40 -0600

    rm todo cruft

--- react/src/actions/index.js
+++ react/src/actions/index.js
@@ -1,27 +1,3 @@
-let nextTodoId = 0
-export const addTodo = text => {
-  return {
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text
-  }
-}
-
-export const setVisibilityFilter = filter => {
-  return {
-    type: 'SET_VISIBILITY_FILTER',
-    filter
-  }
-}
-
-export const toggleTodo = id => {
-  return {
-    type: 'TOGGLE_TODO',
-    id
-  }
-}
-
-
 export const toggleActivity = id => {
   return {
     type: 'TOGGLE_ACTIVITY',
--- react/src/components/App.js
+++ react/src/components/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
 import { VisibleProjectFilter, VisibleActivityList } from '../containers/ActivityList'
 
 const App = () => (
   <div>
     <VisibleProjectFilter />
-    <AddTodo />
-    <VisibleTodoList />
     <VisibleActivityList />
-    <Footer />
   </div>
 )
 
