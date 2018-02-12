0bb4b1581499b5e3417f8868266f51b87f2a37a2
Tony Narlock <tony@git-pull.com>
2018-02-12 14:29:54 -0600

    stopping point, adding graph.js

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { VisibleActorFilter, VisibleActivityList } from '../containers/ActivityList'
+import Graphs from './Graphs.js'
+
 
 const App = () => (
   <div>
     <VisibleActorFilter />
     <VisibleActivityList />
+    <Graphs />
   </div>
 )
 
---
+++ react/src/components/Graphs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Doughnut} from 'react-chartjs-2';
+
+
+class Graphs extends React.Component {
+  render() {
+    return (
+      <span style={{ textAlign: 'left' }}>
+      </span>
+    );
+  }
+}
+
+export default Graphs
