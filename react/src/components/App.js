8d1b5ba5cd9a2bf99d68ab18e36ef81afe8e6f4d
Tony Narlock <tony@git-pull.com>
2018-02-13 12:53:02 -0600

    update header copy

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -7,11 +7,15 @@ const App = () => (
   <div className="header">
     <h1>Tony Narlock's CV</h1>
 
-    <p><em>Friendly note:
-      I don't do technical screens<a href="https://www.git-pull.com/blog/2018/01/technical-screening.html" target="_blank" rel="noopener noreferrer" class="hidden footnote">[1]</a>.
+    <p><em>
       Seeking remote, senior programming roles.
-      I'm also available for <a href="https://www.git-pull.com/consulting/" rel="noopener noreferrer" target="_blank">consulting</a> and
-      <a href="https://www.git-pull.com/mentoring/" rel="noopener noreferrer" target="_blank">mentoring</a>.</em></p>
+      I'm also available for <a href="https://www.git-pull.com/consulting/" rel="noopener noreferrer" target="_blank">consulting</a> and <a href="https://www.git-pull.com/mentoring/" rel="noopener noreferrer" target="_blank">mentoring</a>.
+    </em></p>
+    <p><em>
+      Friendly note:
+      I don't do technical screens. If this can't convince you, I'm probably not good enough for you
+      <a href="https://www.git-pull.com/blog/2018/01/technical-screening.html" target="_blank" rel="noopener noreferrer" class="hidden footnote">[1]</a>.
+    </em></p>
 
     <Graphs />
     <VisibleActorFilter />
