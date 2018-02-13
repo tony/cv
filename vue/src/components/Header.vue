f7dacde80fc75a9a9d9cb004aea78a346443859e
Tony Narlock <tony@git-pull.com>
2018-02-13 13:03:20 -0600

    update copy for title

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -2,11 +2,16 @@
   <div class="header">
     <h1>{{ msg }}</h1>
 
-    <p><em>Friendly note:
-      I don't do technical screens<a href="https://www.git-pull.com/blog/2018/01/technical-screening.html" target="_blank" class="hidden footnote">[1]</a>.
+    <p><em>
       Seeking remote, senior programming roles.
-      I'm also available for <a href="https://www.git-pull.com/consulting/" target="_blank">consulting</a> and
-    <a href="https://www.git-pull.com/mentoring/" target="_blank">mentoring</a>.</em></p>
+      I'm also available for <a href="https://www.git-pull.com/consulting/" target="_blank" rel="noopener noreferrer">consulting</a> and
+      <a href="https://www.git-pull.com/mentoring/" target="_blank" rel="noopener noreferrer">mentoring</a>.
+    </em></p>
+
+    <p><em>
+      Friendly note:
+      I don't do technical screens<a href="https://www.git-pull.com/blog/2018/01/technical-screening.html" target="_blank" rel="noopener noreferrer" class="hidden footnote">[1]</a>. If this can't convince you, I'm probably not good enough for you.
+    </em></p>
 
     <Charts />
     <Lenses />
