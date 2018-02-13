e69efb618d569b0602a576a7cacabad52c5cdc3e
Tony Narlock <tony@git-pull.com>
2018-02-13 12:52:56 -0600

    use header from vue.js

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -4,10 +4,18 @@ import Graphs from './Graphs.js'
 
 
 const App = () => (
-  <div>
+  <div className="header">
+    <h1>Tony Narlock's CV</h1>
+
+    <p><em>Friendly note:
+      I don't do technical screens<a href="https://www.git-pull.com/blog/2018/01/technical-screening.html" target="_blank" rel="noopener noreferrer" class="hidden footnote">[1]</a>.
+      Seeking remote, senior programming roles.
+      I'm also available for <a href="https://www.git-pull.com/consulting/" rel="noopener noreferrer" target="_blank">consulting</a> and
+      <a href="https://www.git-pull.com/mentoring/" rel="noopener noreferrer" target="_blank">mentoring</a>.</em></p>
+
+    <Graphs />
     <VisibleActorFilter />
     <VisibleActivityList />
-    <Graphs />
   </div>
 )
 
