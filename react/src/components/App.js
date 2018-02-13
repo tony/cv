9289055ebe0013f61cd6f672eaf5a9070d3172f0
Tony Narlock <tony@git-pull.com>
2018-02-13 13:19:14 -0600

    fix language-tag color, root html structure, className

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -4,21 +4,24 @@ import Graphs from './Graphs.js'
 
 
 const App = () => (
-  <div className="header">
-    <h1>Tony Narlock's CV</h1>
+  <div id="app">
+    <div className="header">
+      <h1>Tony Narlock's CV</h1>
 
-    <p><em>
-      Seeking remote, senior programming roles.
-      I'm also available for <a href="https://www.git-pull.com/consulting/" rel="noopener noreferrer" target="_blank">consulting</a> and <a href="https://www.git-pull.com/mentoring/" rel="noopener noreferrer" target="_blank">mentoring</a>.
-    </em></p>
-    <p><em>
-      Friendly note:
-      I don't do technical screens. If this can't convince you, I'm probably not good enough for you
-      <a href="https://www.git-pull.com/blog/2018/01/technical-screening.html" target="_blank" rel="noopener noreferrer" class="hidden footnote">[1]</a>.
-    </em></p>
+      <p><em>
+        Seeking remote, senior programming roles.
+        I'm also available for <a href="https://www.git-pull.com/consulting/" rel="noopener noreferrer" target="_blank">consulting</a> and <a href="https://www.git-pull.com/mentoring/" rel="noopener noreferrer" target="_blank">mentoring</a>.
+      </em></p>
+      <p><em>
+        Friendly note:
+        I don't do technical screens. If this can't convince you, I'm probably not good enough for you
+        <a href="https://www.git-pull.com/blog/2018/01/technical-screening.html" target="_blank" rel="noopener noreferrer" className="hidden footnote">[1]</a>.
+      </em></p>
 
-    <Graphs />
-    <VisibleActorFilter />
+      <Graphs />
+      <VisibleActorFilter />
+
+    </div>
     <VisibleActivityList />
   </div>
 )
