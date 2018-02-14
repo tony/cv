0c9e6a68ca5815b253c7c83fff4399f43592120e
Tony Narlock <tony@git-pull.com>
2018-02-14 17:01:26 -0600

    rm labels from octicons on vue

--- vue/src/components/activities/Software/partials/Links.vue
+++ vue/src/components/activities/Software/partials/Links.vue
@@ -2,17 +2,17 @@
   <div class="software-project-links">
     <a v-show="item.actor.url" :href="item.actor.url"
      target="_blank" class="align-octicon">
-      <octicon name="home" label="Created"></octicon>
+      <octicon name="home"></octicon>
       Website
     </a>
     <a v-show="item.actor.repo_url" :href="item.actor.repo_url"
      target="_blank" class="align-octicon">
-      <octicon name="mark-github" label="Created"></octicon>
+      <octicon name="mark-github"></octicon>
       GitHub
     </a>
     <a v-show="item.actor.docs_url" :href="item.actor.docs_url"
      target="_blank" class="align-octicon">
-      <octicon name="book" label="Created"></octicon>
+      <octicon name="book"></octicon>
       Docs
     </a>
     <a v-show="item.actor.api_url" :href="item.actor.api_url"
@@ -22,17 +22,17 @@
     </a>
     <a v-show="item.actor.coverage_url" :href="item.actor.coverage_url"
      target="_blank" class="align-octicon">
-      <octicon name="graph" label="Created"></octicon>
+      <octicon name="graph"></octicon>
       Coverage
     </a>
     <a v-show="item.actor.ci_url" :href="item.actor.ci_url"
      target="_blank" class="align-octicon">
-      <octicon name="dashboard" label="Created"></octicon>
+      <octicon name="dashboard"></octicon>
       CI
     </a>
     <a v-show="item.actor.browse_code_url"
       :href="item.actor.browse_code_url" target="_blank" class="align-octicon">
-      <octicon name="code" label="Created"></octicon>
+      <octicon name="code"></octicon>
       Browse Code
     </a>
     <a v-show="item.actor.browse_code_tests_url"
@@ -41,12 +41,12 @@
     </a>
     <a v-show="item.actor.issues_url"
         :href="item.actor.issues_url" target="_blank" class="align-octicon">
-      <octicon name="comment-discussion" label="Created"></octicon>
+      <octicon name="comment-discussion"></octicon>
       Issues
     </a>
     <a v-show="item.actor.changelog_url"
         :href="item.actor.changelog_url" target="_blank" class="align-octicon">
-      <octicon name="list-unordered" label="Created"></octicon>
+      <octicon name="list-unordered"></octicon>
       Changelog
     </a>
   </div>
