00d24a1a7d8ee3411053c70a1cc4de746a8941fe
Tony Narlock <tony@git-pull.com>
2018-02-14 20:25:11 -0600

    get images working across generic components

--- react/src/components/ActivityList.js
+++ react/src/components/ActivityList.js
@@ -36,6 +36,20 @@ class RightBox extends React.Component {
   render() {
     return (
       <div className="box">
+        { this.props.actor.logo && this.props.actor.logo.length > 0 &&
+            <a href={this.props.actor.url}
+              target="_blank"
+              className="align-octicon"
+            >
+              <img
+                src={this.props.actor.logo}
+                className="logo"
+                style={{ maxWidth: 200 }}
+                alt={this.props.actor.name}
+              />
+            </a>
+        }
+
         <h3><a href={this.props.qa_url} target="_blank" className="activity-title">{this.props.title}</a></h3>
         <p>
           <em>was contributed to the <a href={this.props.actor.url} className='muted-link'>{this.props.actor.name}</a> project</em>
