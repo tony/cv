a9b076c4cf2546d374a5597d385c178df8e1f9af
Tony Narlock <tony@git-pull.com>
2018-02-27 16:25:36 -0600

    hide note about consulting/mentoring/seeking roles

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <h1>{{ msg }}</h1>
 
-    <p><em>
+    <p class="hidden"><em>
       Seeking remote, senior programming roles.
       I'm also available for <a href="https://www.git-pull.com/consulting/" target="_blank" rel="noopener noreferrer">consulting</a> and
       <a href="https://www.git-pull.com/mentoring/" target="_blank" rel="noopener noreferrer">mentoring</a>.
