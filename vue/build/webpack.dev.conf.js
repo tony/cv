164af704cc276115b7727c17d7bb16874afa79bd
Tony Narlock <tony@git-pull.com>
2018-08-26 12:43:26 -0400

    Remove postcss for now
    vue-loader removes this in the most recent version. Add back later

--- vue/build/webpack.dev.conf.js
+++ vue/build/webpack.dev.conf.js
@@ -14,8 +14,9 @@ const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'development',
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: false })
   },
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
