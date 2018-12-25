f1d5204d10d4c4655aaf55f11172e72581b81741
Tony Narlock <tony@git-pull.com>
2018-12-25 17:39:40 -0600

    react: Add Initial state from v1, redux-logger, react-redux

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,24 +1,53 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import "./App.scss";
 import { CVActivity } from "./lib/types";
 import cvReducers from "./reducers";
 
+import {
+  activityTypes,
+  INITIAL_DATA,
+  DEFAULT_SELECTED_FILTERS
+} from "./lib/constants";
+import { filterMap } from "./lib/selectors";
+
 const myActivities: Array<CVActivity> = require("./data/my_activities.json");
+
+const logger = createLogger({
+  // ...options
+});
+let store = createStore(
+  cvReducers,
+  {
+    ...INITIAL_DATA.entities,
+    activityTypes,
+    selectedActors: [],
+    selectedActivityTypes: [],
+    selectedFilters: DEFAULT_SELECTED_FILTERS,
+    selectedLanguages: [],
+    filters: filterMap
+  },
+  applyMiddleware(logger)
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>Tony Narlock's CV</p>
-          {myActivities.length}
-          {myActivities.map((activity, idx) => (
-            <p key={idx}>
-              On {activity.created_date} Tony {activity.title}
-            </p>
-          ))}
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <p>Tony Narlock's CV</p>
+            {myActivities.length}
+            {myActivities.map((activity, idx) => (
+              <p key={idx}>
+                On {activity.created_date} Tony {activity.title}
+              </p>
+            ))}
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
