522d3ca90dd0d96065691a34441d8adce53ad9a4
Tony Narlock <tony@git-pull.com>
2018-12-25 17:42:24 -0600

    react: Split store into its own module

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,36 +1,11 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
+
 import "./App.scss";
 import { CVActivity } from "./lib/types";
-import cvReducers from "./reducers";
-
-import {
-  activityTypes,
-  INITIAL_DATA,
-  DEFAULT_SELECTED_FILTERS
-} from "./lib/constants";
-import { filterMap } from "./lib/selectors";
+import { store } from "./store";
 
 const myActivities: Array<CVActivity> = require("./data/my_activities.json");
-
-const logger = createLogger({
-  // ...options
-});
-let store = createStore(
-  cvReducers,
-  {
-    ...INITIAL_DATA.entities,
-    activityTypes,
-    selectedActors: [],
-    selectedActivityTypes: [],
-    selectedFilters: DEFAULT_SELECTED_FILTERS,
-    selectedLanguages: [],
-    filters: filterMap
-  },
-  applyMiddleware(logger)
-);
 
 class App extends Component {
   render() {
---
+++ react/src/store.ts
@@ -0,0 +1,27 @@
+import {
+  activityTypes,
+  INITIAL_DATA,
+  DEFAULT_SELECTED_FILTERS
+} from "./lib/constants";
+import { filterMap } from "./lib/selectors";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import cvReducers from "./reducers";
+
+const logger = createLogger({
+  // ...options
+});
+
+export var store = createStore(
+  cvReducers,
+  {
+    ...INITIAL_DATA.entities,
+    activityTypes,
+    selectedActors: [],
+    selectedActivityTypes: [],
+    selectedFilters: DEFAULT_SELECTED_FILTERS,
+    selectedLanguages: [],
+    filters: filterMap
+  },
+  applyMiddleware(logger)
+);
