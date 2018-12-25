84273ce006433fdfcc1b98198b13e78f5502525b
Tony Narlock <tony@git-pull.com>
2018-12-25 11:32:13 -0600

    react: Add typing for activity, print out

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.scss";
+const path = require("path");
+const process = require("process");
+
+interface IActivity {
+  id: string;
+  component_name: string;
+  created_date: string;
+  title: string;
+  actor: Number;
+}
+
+const myActivities: Array<IActivity> = require("./data/my_activities.json");
+console.log({ myActivities });
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Tony Narlock's CV</p>
+          {myActivities.length}
+          {myActivities.map(activity => (
+            <p>
+              On {activity.created_date} Tony {activity.title}
+            </p>
+          ))}
         </header>
       </div>
     );
