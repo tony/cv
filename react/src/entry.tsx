262c65b8d93e55e648b8c29f0cb7efe4b2678530
Tony Narlock <tony@git-pull.com>
2019-07-04 12:05:44 -0500

    react: Update for new export sig

--- react/src/entry.tsx
+++ react/src/entry.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-import myActivities from "../../lib/data";
+import { myActivities } from "../../lib/data";
 
 console.log({ myActivities });
 
