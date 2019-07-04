2d03bc334bacd345b04414a6a607d3dd56561d74
Tony Narlock <tony@git-pull.com>
2019-07-04 12:05:27 -0500

    lib/data: Structure data import/naming

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,8 @@
-import rawActivities from "../data/my_activities.json";
+import handPickedActivitiesRaw from "../data/my_activities.json";
+import handPickedActorsRaw from "../data/my_actors.json";
+import ghActivitiesRaw from "../data/scraped/gh_activities.json";
+import ghActorsRaw from "../data/scraped/gh_actors.json";
 
 // Join JSON files into a normal list
 
-const myData = [...rawActivities];
-
-export default myData;
+export const myActivities = [...handPickedActivitiesRaw, ...ghActivitiesRaw];
