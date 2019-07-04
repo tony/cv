b4244178b6267eb3272d9d7e919625aee23e4020
Tony Narlock <tony@git-pull.com>
2019-07-04 09:19:55 -0500

    react: Local from out of project module

--- react/src/entry.tsx
+++ react/src/entry.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App.tsx";
+import App from "./App";
+
+import myActivities from "../../lib/data";
+
+console.log({ myActivities });
 
 ReactDOM.render(<App />, document.getElementById("root"));
