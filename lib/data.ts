51e647dc19b00395cd5578820fd45c9cecf262c5
Tony Narlock <tony@git-pull.com>
2019-07-05 11:52:39 -0500

    lib: Add IActivity type

--- lib/data.ts
+++ lib/data.ts
@@ -3,6 +3,10 @@ import handPickedActorsRaw from "../data/my_actors.json";
 import ghActivitiesRaw from "../data/scraped/gh_activities.json";
 import ghActorsRaw from "../data/scraped/gh_actors.json";
 
-// Join JSON files into a normal list
+import { IActivity } from "./types";
 
-export const myActivities = [...handPickedActivitiesRaw, ...ghActivitiesRaw];
+// Join JSON files into a normal list
+export const myActivities: IActivity[] = [
+  ...handPickedActivitiesRaw,
+  ...ghActivitiesRaw
+];
---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export interface IActivity {
+  created_date: string;
+  title: string;
+  actor: number;
+  accepted_date?: string;
+  qa_url?: string;
+  diff_url?: string;
+  end_date?: string;
+  start_date?: string;
+}
