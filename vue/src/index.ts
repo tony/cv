c362aa465373b6d3994d4c94435d96e808941a15
Tony Narlock <tony@git-pull.com>
2019-07-05 11:23:24 -0500

    vue: Load activities async

--- vue/src/index.ts
+++ vue/src/index.ts
@@ -1,26 +1,47 @@
 import Vue from "vue";
+import Vuex, { StoreOptions } from "vuex";
+
 import HelloComponent from "./components/Hello.vue";
 import HelloDecoratorComponent from "./components/HelloDecorator.vue";
 
-(async () => {
-  try {
-    const { myActivities } = await import(
-      /* webpackChunkName: "myData" */ "../../lib/data"
-    );
+interface IRootState {
+  activities: any[];
+}
+Vue.use(Vuex);
 
-    console.log({ myActivities });
-  } catch (e) {
-    console.error(e);
+const store: StoreOptions<IRootState> = {
+  actions: {
+    async loadActivities({ commit }, activities) {
+      const { myActivities } = await import(
+        /* webpackChunkName: "myData" */ "../../lib/data"
+      );
+      commit("loadActivities", myActivities);
+    }
+  },
+  mutations: {
+    loadActivities(state, activities) {
+      state.activities = activities;
+    }
+  },
+  state: {
+    activities: []
   }
-})();
+};
 
+const s = new Vuex.Store<IRootState>(store);
 const v = new Vue({
+  created() {
+    console.log("created");
+    s.dispatch("loadActivities");
+  },
   components: {
     HelloComponent,
     HelloDecoratorComponent
   },
+  computed: { ...Vuex.mapState(["activities"]) },
   data: { name: "World" },
   el: "#root",
+  store: s,
   template: `
     <div>
         Name: <input v-model="name" type="text">
@@ -28,6 +49,11 @@ const v = new Vue({
         <hello-component :name="name" :initialEnthusiasm="5" />
         <h1>Hello Decorator Component</h1>
         <hello-decorator-component :name="name" :initialEnthusiasm="5" />
+          <div v-for="(activity, idx) in activities">
+            {{activity.title}}
+          </div>
         </div>
     `
 });
+
+export default s;
