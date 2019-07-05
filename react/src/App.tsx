e4e315d327c2abb8d3c5ce416bfe01d65670622d
Tony Narlock <tony@git-pull.com>
2019-07-05 09:58:11 -0500

    react: Functional component + hooks + lazy loaded data

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,40 @@
 import React from "react";
 
-const App: React.FC = () => {
+interface IState {
+  myActivities: [];
+}
+interface IActivity {
+  created_date: string;
+  title: string;
+  actor: number;
+  accepted_date?: string;
+  qa_url?: string;
+  diff_url?: string;
+  end_date?: string;
+  start_date?: string;
+}
+
+const App: React.FC<IState> = () => {
+  const [activities, setActivities] = React.useState<IActivity[] | null>([]);
+  const fetchActivities = async () => {
+    const { myActivities } = await import(
+      /* webpackChunkName: "myData" */ "../../lib/data"
+    );
+    setActivities(myActivities as IActivity[]);
+  };
+  React.useEffect(() => {
+    fetchActivities();
+  });
+
+  console.log("renderState", { activities });
+
   return (
     <div>
       <header>
+        {activities &&
+          activities.map((activity, idx) => (
+            <div key={idx}>{activity.title}</div>
+          ))}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- react/src/entry.tsx
+++ react/src/entry.tsx
@@ -3,16 +3,4 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-(async () => {
-  try {
-    const { myActivities } = await import(
-      /* webpackChunkName: "myData" */ "../../lib/data"
-    );
-
-    console.log({ myActivities });
-  } catch (e) {
-    console.error(e);
-  }
-})();
-
 ReactDOM.render(<App />, document.getElementById("root"));
