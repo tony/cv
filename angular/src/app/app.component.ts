00d0962243f487fed6c258f649b975291cfb7cb8
Tony Narlock <tony@git-pull.com>
2019-07-05 20:21:29 -0500

    angular: Stub out activities (lazy loaded)

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { IActivity } from "@lib/types";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular';
+  title = "angular";
+  activities: IActivity[] = [];
+  async ngOnInit() {
+    console.log("ngOnInit");
+    const { myActivities } = await import(
+      /* webpackChunkName: "myData" */ "@lib/data"
+    );
+    console.log({ myActivities });
+    this.activities = myActivities;
+  }
 }
