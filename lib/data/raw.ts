bde24f37f1cfd732b36d108e02109bfcab1a855a
Tony Narlock <tony@git-pull.com>
2019-07-06 14:24:28 -0500

    wip: Trial normalizr

--- lib/data/raw.ts
+++ lib/data/raw.ts
@@ -1,3 +1,5 @@
+import { normalize, schema } from "normalizr";
+
 import handPickedActivitiesRaw from "../../data/my_activities.json";
 import handPickedActorsRaw from "../../data/my_actors.json";
 import ghActivitiesRaw from "../../data/scraped/gh_activities.json";
@@ -15,3 +17,41 @@ export const myActivitiesRaw: IActivity[] = [
   ...handPickedActivitiesRaw,
   ...ghActivitiesRaw
 ];
+
+const language = new schema.Entity(
+  "language",
+  {},
+  { idAttribute: value => value.componentName }
+);
+
+const actor = new schema.Entity(
+  "actor",
+  { languages: [language] },
+  {
+    idAttribute: (value, parentr, key) => {
+      console.log({ value, parentr, key });
+      return value.id;
+    }
+  }
+);
+
+const activityType = new schema.Entity(
+  "activityTypes",
+  {},
+  {
+    idAttribute: v => v.componentName
+  }
+);
+
+const activity = new schema.Entity("activity", {
+  actor,
+  componentName: activityType
+});
+
+const normalizeData = normalize(
+  myActivitiesRaw,
+  // { actor: myActorsRaw, activities: myActivitiesRaw, componentName: {} },
+  [activity]
+);
+
+console.log({ normalizeData });
