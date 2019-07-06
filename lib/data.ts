39f08daa64da8057274cfe935f469533429d62e7
Tony Narlock <tony@git-pull.com>
2019-07-06 09:22:10 -0500

    data: Upcast types (needed for literals)

--- lib/data.ts
+++ lib/data.ts
@@ -6,7 +6,12 @@ import ghActorsRaw from "../data/scraped/gh_actors.json";
 import { IActivity, IActor } from "./types";
 
 // Join JSON files into a normal list
-export const myActors: IActor[] = [...handPickedActorsRaw, ...ghActorsRaw];
+export const myActors: IActor[] = [
+  ...(handPickedActorsRaw as IActor[]),
+  ...(ghActorsRaw as IActor[])
+];
+
+// print all languages: new Set([].concat(...myActors.map(a => a.languages)))
 
 export const myActivitiesRaw: IActivity[] = [
   ...handPickedActivitiesRaw,
