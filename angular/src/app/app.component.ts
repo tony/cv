77fd211cf351c1854c1f35c262617ed1c8432219
Tony Narlock <tony@git-pull.com>
2019-07-05 23:41:52 -0500

    angular: Prettier and add OnInit

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,19 +1,17 @@
-import { Component } from "@angular/core";
-import { IActivity } from "@lib/types";
+import { Component, OnInit } from '@angular/core';
+import { IActivity } from '@lib/types';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = "angular";
+export class AppComponent implements OnInit {
+  title = 'angular';
   activities: IActivity[] = [];
   async ngOnInit() {
-    console.log("ngOnInit");
-    const { myActivities } = await import(
-      /* webpackChunkName: "myData" */ "@lib/data"
-    );
+    console.log('ngOnInit');
+    const { myActivities } = await import(/* webpackChunkName: "myData" */ '@lib/data');
     console.log({ myActivities });
     this.activities = myActivities;
   }
