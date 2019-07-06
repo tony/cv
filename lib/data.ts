abec4e567fdf6ccee13b429301f33e0e501550af
Tony Narlock <tony@git-pull.com>
2019-07-06 08:26:53 -0500

    lib: Export actor data

--- lib/data.ts
+++ lib/data.ts
@@ -3,10 +3,12 @@ import handPickedActorsRaw from "../data/my_actors.json";
 import ghActivitiesRaw from "../data/scraped/gh_activities.json";
 import ghActorsRaw from "../data/scraped/gh_actors.json";
 
-import { IActivity } from "./types";
+import { IActivity, IActor } from "./types";
 
 // Join JSON files into a normal list
-export const myActivities: IActivity[] = [
+export const myActors: IActor[] = [...handPickedActorsRaw, ...ghActorsRaw];
+
+export const myActivitiesRaw: IActivity[] = [
   ...handPickedActivitiesRaw,
   ...ghActivitiesRaw
 ];
