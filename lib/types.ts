97cc451879978ef29cc8c4823e7aae7d050aed06
Tony Narlock <tony@git-pull.com>
2019-07-06 08:26:35 -0500

    Add types for actors

--- lib/types.ts
+++ lib/types.ts
@@ -8,3 +8,25 @@ export interface IActivity {
   end_date?: string;
   start_date?: string;
 }
+
+type ActorType = "Open Source" | "Company" | "Publication" | "Website";
+type ActorLanguage = "JavaScript" | "Python" | "PHP";
+
+export interface IActor {
+  id: number;
+  actorType: string;
+  name: string;
+  url: string;
+  old_url?: string;
+  repo_url?: string;
+  docs_url?: string;
+  api_url?: string;
+  ci_url?: string;
+  coverage_url?: string;
+  changelog_url?: string;
+  issues_url?: string;
+  browse_code_tests_url?: string;
+  browse_code_url?: string;
+  logo?: string;
+  languages: ActorLanguage[];
+}
