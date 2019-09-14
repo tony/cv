9fd6af0879f30398cd86f7f6c830ba4d6c725207
Tony Narlock <tony@git-pull.com>
2019-09-14 16:27:13 -0500

    search: Refactor lenses to handle filtering, values

--- lib/search/lense.ts
+++
@@ -1,6 +0,0 @@
-export type PredicateFn<ValueT> = (value: ValueT) => boolean;
-
-export interface ILense<T> {
-  filterFn: PredicateFn<T>;
-  label: string;
-}
--- lib/search/search.ts
+++ lib/search/search.ts
@@ -9,7 +9,6 @@ import {
   IActor,
   IActors
 } from "../types";
-import { ILense } from "./lense";
 
 interface IStateData {
   activities: IActivity[];
@@ -19,9 +18,43 @@ interface IStateData {
   languages: ActorLanguage[];
 }
 
+export type FilterFn<ValueT> = (
+  value: ValueT,
+  activity: IActivity,
+  data: IStateData
+) => boolean;
+export type LenseType = keyof IStateData;
+export interface ISearchFilters {
+  [LenseType: string]: FilterFn<any>;
+  languages: FilterFn<ActorLanguage>;
+}
+
+interface ILenses {
+  activityTypes: Set<string>;
+  actorTypes: Set<string>;
+  actors: Set<string>;
+  languages: Set<ActorLanguage>;
+}
+
+// const SearchFilters: Map<LenseType, FilterFn<any>> = new Map({
+const SearchFilters: ISearchFilters = {
+  languages: (value, activity, data) => {
+    const actor = data.actors[activity.actorId];
+    if (!actor || !actor.languages || !actor.languages.length) {
+      return false;
+    }
+    return actor.languages.includes(value);
+  }
+};
+
 export class Search<ValueT> {
   public data: Readonly<IStateData>;
-  public lenses: Array<ILense<any>> = [];
+  public lenses: ILenses = {
+    activityTypes: new Set(),
+    actorTypes: new Set(),
+    actors: new Set(),
+    languages: new Set()
+  };
 
   //
   // Parent (creating) state. null for root-level
@@ -53,18 +86,16 @@ export class Search<ValueT> {
     };
   }
 
-  public lenseExists(lense: ILense<any>) {
-    return this.lenses.some(oldLense => lense.label === oldLense.label);
+  public lenseExists(lenseType, value) {
+    return this.lenses[lenseType].has(value);
   }
 
-  public addLense(lense: ILense<any>) {
-    if (typeof lense.filterFn !== "function") {
-      throw new Error(
-        "Expected 'filterFn' param to 'Search.addLense' method to be a function."
-      );
-    }
+  public addLense(lenseType, value) {
+    this.lenses[lenseType].add(value);
+  }
 
-    this.lenses.push(lense);
+  public deleteLense(lenseType, value) {
+    this.lenses[lenseType].delete(value);
   }
 
   public getResults(): IStateData {
@@ -77,9 +108,13 @@ export class Search<ValueT> {
       }),
       {}
     );
-    for (const lense of this.lenses) {
+    for (const language of Array.from(this.lenses.languages.values())) {
       // append behavior, so adding a language widens scope to an additional language
-      activities = activities.concat(initialActivities.filter(lense.filterFn));
+      activities = activities.concat(
+        initialActivities.filter(activity => {
+          return SearchFilters.languages(language, activity, this.data);
+        })
+      );
     }
     return {
       activities,
