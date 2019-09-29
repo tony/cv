4878a9432b40e17943c58e8a06d71bdfc32df236
Tony Narlock <tony@git-pull.com>
2019-09-29 10:06:56 -0500

    lib/search test: describe, add beforeEach example

--- lib/search/search.test.ts
+++ lib/search/search.test.ts
@@ -1,12 +1,31 @@
 import { Search } from "./search";
 
-test("init", () => {
-  const s = new Search();
-  expect(s.data.activities).toStrictEqual([]);
+describe("object initialization", () => {
+  test("init", () => {
+    const s = new Search();
+    expect(s.data.activities).toStrictEqual([]);
+  });
+
+  test("fetch", async () => {
+    const data = await import(
+      /* webpackChunkName: "myData" */ "../../lib/data"
+    );
+    const s = new Search(data);
+    expect(s.data.activities).not.toStrictEqual([]);
+  });
 });
 
-test("fetch", async () => {
-  const data = await import(/* webpackChunkName: "myData" */ "../../lib/data");
-  const s = new Search(data);
-  expect(s.data.activities).not.toStrictEqual([]);
+describe("test with data", () => {
+  let search;
+  beforeEach(async () => {
+    const data = await import(
+      /* webpackChunkName: "myData" */ "../../lib/data"
+    );
+    search = new Search(data);
+    return search;
+  });
+
+  test("search has data loaded for beforerEach", () => {
+    expect(search.data.activities).not.toStrictEqual([]);
+  });
 });
