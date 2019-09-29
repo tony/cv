e86af515bab519ee71157e43ace1ee7c44d36eff
Tony Narlock <tony@git-pull.com>
2019-09-29 09:21:04 -0500

    lib/search: Test loading data

--- lib/search/search.test.ts
+++ lib/search/search.test.ts
@@ -4,3 +4,26 @@ test("init", () => {
   const s = new Search();
   expect(s.data.activities).toStrictEqual([]);
 });
+
+test("fetch", async () => {
+  const fetchActivities = async () => {
+    return import(/* webpackChunkName: "myData" */ "../../lib/data");
+  };
+  const {
+    activities,
+    actors,
+    languages,
+    actorTypes,
+    activityTypes
+  } = await fetchActivities();
+
+  const s = new Search();
+  s.setState({
+    activities,
+    activityTypes,
+    actorTypes,
+    actors,
+    languages
+  });
+  expect(s.data.activities).not.toStrictEqual([]);
+});
