f218840996ec05d0b3e125226bd1072f6702d37d
Tony Narlock <tony@git-pull.com>
2019-12-01 11:27:10 -0600

    angular: Lint karma config

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -1,10 +1,22 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
+    autoWatch: true,
     basePath: '',
+    browsers: ['Chrome'],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    colors: true,
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/angular'),
+      fixWebpackSourcePaths: true
+      reports: ['html', 'lcovonly', 'text-summary'],
+    },
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    logLevel: config.LOG_INFO,
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -12,21 +24,9 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    reporters: ['progress', 'kjhtml'],
     restartOnFileChange: true
+    singleRun: false,
   });
 };
