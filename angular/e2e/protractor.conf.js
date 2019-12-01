9c4e84cf876ffda07c6d3b36513dc9235abe7efd
Tony Narlock <tony@git-pull.com>
2019-12-01 11:25:33 -0600

    angular: lint protractor

--- angular/e2e/protractor.conf.js
+++ angular/e2e/protractor.conf.js
@@ -9,24 +9,22 @@ const { SpecReporter } = require('jasmine-spec-reporter');
  */
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
+  baseUrl: 'http://localhost:4200/',
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
-    showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: () => undefined,
+    showColors: true
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+  },
+  specs: ['./src/**/*.e2e-spec.ts']
 };
