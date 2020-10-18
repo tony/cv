3d76d13dd55b4f8d3836b17cb0f4c0c05ec0bc2c
Tony Narlock <tony@git-pull.com>
2020-10-18 11:18:03 -0500

    can delete, wip on fixing refresh on react with webpack v5

--- react/webpack/webpack.config.ts
+++ react/webpack/webpack.config.ts
@@ -2,7 +2,7 @@ import path from "path";
 
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
-
+import ReactRefreshPlugin from "@webhotelier/webpack-fast-refresh";
 const projectRoot = path.join(__dirname, "../");
 
 interface IWebpackEnv {
@@ -21,117 +21,105 @@ const defaultEnvironment: IWebpackEnv = {
   watch: false,
 };
 
-const getConfig = (env: IWebpackEnv): webpack.Configuration => ({
-  context: projectRoot,
-  ...(process.argv.some((arg) => arg.includes("webpack-dev-server"))
-    ? {
-        devServer: {
-          contentBase: "./dist",
-          host: env.devServerHost,
-          hot: true,
-          open: env.open,
-          port: parseInt(env.devServerPort, 10),
-          publicPath: "/",
-        },
-      }
-    : {}),
-  devtool: env.production ? "source-map" : "inline-source-map",
-  entry: {
-    cv: [
-      ...(process.argv.some((arg) => arg.includes("webpack-dev-server"))
-        ? [
-            `webpack-dev-server/client?http://${env.devServerHost}:${env.devServerPort}`,
-            "webpack/hot/dev-server",
-          ]
-        : []),
-      "./src/entry.tsx",
-    ] as [string, ...string[]],
-  },
-  mode: env.production ? "production" : "development",
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.tsx$/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            babelrc: true,
-            configFile: "./.babelrc",
-          },
-        },
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.ts$/,
-        use: {
-          loader: "ts-loader",
-        },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              // Prefer `dart-sass`
-              implementation: require("sass"),
-            },
-          },
-        ],
-      },
+const getConfig = (env: IWebpackEnv): webpack.Configuration => {
+  const isWebpackServer = process.argv.includes("serve");
+  console.log({ env, argv: process.argv, isWebpackServer });
 
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "svg-url-loader",
-            options: {
-              limit: 10000,
-            },
+  return {
+    context: projectRoot,
+    ...(isWebpackServer
+      ? {
+          devServer: {
+            contentBase: "./dist",
+            host: env.devServerHost,
+            hot: true,
+            open: env.open,
+            port: parseInt(env.devServerPort, 10),
+            publicPath: "/",
           },
-        ],
-      },
-    ],
-  },
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          // @ts-nocheck-next-line
-          name(mod: webpack.DynamicEntryPlugin) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = mod?.context.match(
-              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-            )[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `vendor.${packageName.replace("@", "")}`;
-          },
-        },
-      },
-      chunks: "all",
-      maxInitialRequests: Infinity,
-      minSize: 0,
+        }
+      : {}),
+    devtool: env.production ? "source-map" : "inline-source-map",
+    entry: {
+      cv: [
+        ...(isWebpackServer
+          ? [
+              "@webhotelier/webpack-fast-refresh/runtime.js",
+              `webpack-dev-server/client?http://${env.devServerHost}:${env.devServerPort}`,
+            ]
+          : []),
+        "./src/entry.tsx",
+      ] as [string, ...string[]],
     },
-  },
-  output: {
-    chunkFilename: "[name].[chunkhash].js",
-    filename: "[name].[hash].js",
-    path: path.resolve(projectRoot, "dist"),
-  },
-  plugins: [new HtmlWebpackPlugin({ template: "../lib/assets/index.html" })],
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  stats: { children: false },
-  watch: env.watch,
-});
+    mode: env.production ? "production" : "development",
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /\.tsx$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              babelrc: true,
+              configFile: "./.babelrc",
+            },
+          },
+        },
+        {
+          exclude: /node_modules/,
+          test: /\.ts$/,
+          use: {
+            loader: "ts-loader",
+          },
+        },
+        {
+          test: /\.tsx$/,
+          loader: "@webhotelier/webpack-fast-refresh/loader.js",
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                // Prefer `dart-sass`
+                implementation: require("sass"),
+              },
+            },
+          ],
+        },
+
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "svg-url-loader",
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    output: {
+      chunkFilename: "[name].[chunkhash].js",
+      filename: "[name].[hash].js",
+      path: path.resolve(projectRoot, "dist"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "../lib/assets/index.html" }),
+      ...(isWebpackServer ? [new ReactRefreshPlugin()] : []),
+    ],
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    stats: { children: false },
+    watch: env.watch,
+  };
+};
 
 export default (
   env: IWebpackEnv // Merge default environment params
