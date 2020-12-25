4562edbc14785141c464391fade33a38aa7f041d
Tony Narlock <tony@git-pull.com>
2020-12-25 13:33:39 -0600

    react: Stricter typings (add ts typings)

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -8,14 +8,20 @@ module.exports = {
   globals: {
     $$queries: "readable",
   },
-  extends: ["plugin:react/recommended", "eslint:recommended"],
+  extends: [
+    "plugin:react/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 2020,
     sourceType: "module",
+    project: "./tsconfig.json",
   },
   plugins: ["react", "@typescript-eslint", "jest"],
   settings: {
