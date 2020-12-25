46cb0b403c9f1d738713fc046bebd85887bd0981
Tony Narlock <tony@git-pull.com>
2020-12-25 10:34:01 -0600

    react: Move react-select stuff to separate module

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Select, { components as ReactSelectComponents } from "react-select";
+import Select from "react-select";
 import type { Subscription, Observable } from "rxjs";
 import { ActionMeta, OptionProps, ValueType } from "react-select/src/types"; // tslint:disable-line no-submodule-imports
-import chroma from "chroma-js";
 
 import {
   ActivityType,
@@ -25,71 +24,11 @@ import {
   languagesStore,
   languagesQuery,
 } from "../../lib/hub";
-import { getSelectOptions } from "./react-select";
+import { getSelectOptions, languagesStyles, OrgOption } from "./react-select";
 import type { ISelectOption, IOptionType } from "./react-select";
 import { useAsyncEffect } from "./utils";
 
 import "./style.scss";
-
-interface IActivityCardProps {
-  activity: IActivity;
-  org: IOrg;
-}
-
-const OrgOption = ({ children, data, ...props }) => {
-  const org = $$queries.orgs.getEntity(data?.value);
-  return (
-    <ReactSelectComponents.Option {...props}>
-      {children}{" "}
-      {org?.orgType && (
-        <>
-          <div
-            style={{
-              display: "inline-flex",
-              fontSize: ".75rem",
-              lineHeight: 1.5,
-              whiteSpace: "nowrap",
-              color: "#4a4a4a",
-              backgroundColor: "#f5f5f5",
-              padding: ".3rem .5rem",
-              borderRadius: ".3rem",
-            }}
-          >
-            {org.orgType}
-          </div>
-
-          {org.languages.map((languageId) => {
-            const language = $$queries.languages.getEntity(languageId);
-            if (!language) {
-              return null;
-            }
-            return (
-              <div
-                style={{
-                  display: "inline-flex",
-                  fontSize: ".75rem",
-                  lineHeight: 1.5,
-                  whiteSpace: "nowrap",
-                  color: language?.color
-                    ? chroma(language?.color).get("lab.l") > 70
-                      ? "black"
-                      : "white"
-                    : "#4a4a4a",
-                  backgroundColor: language?.color ?? "#f5f5f5",
-                  padding: ".3rem .5rem",
-                  marginLeft: ".5rem",
-                  borderRadius: ".3rem",
-                }}
-              >
-                {languageId}
-              </div>
-            );
-          })}
-        </>
-      )}
-    </ReactSelectComponents.Option>
-  );
-};
 
 function onEmit<T>(
   source$: Observable<T>,
@@ -98,62 +37,10 @@ function onEmit<T>(
   return source$.subscribe(nextFn, console.error);
 }
 
-const languagesStyles = {
-  option: (styles, { data }) => {
-    const language = $$queries.languages.getEntity(data.value);
-    if (!language?.color && !language?.textColor) {
-      return styles;
-    }
-    const backgroundColor = chroma(language.color).alpha(0.5).css();
-    return {
-      ...styles,
-      backgroundColor,
-      color: chroma(backgroundColor).get("lab.l") > 80 ? "black" : "white",
-      "&:hover": {
-        backgroundColor: "#DEEBFF",
-      },
-    };
-  },
-
-  multiValue: (styles, { data }) => {
-    const language = $$queries.languages.getEntity(data.value);
-    // const color = chroma(data.color);
-    return {
-      ...styles,
-      // backgroundColor: color.alpha(0.1).css(),
-      backgroundColor: language.color,
-    };
-  },
-  multiValueLabel: (styles, { data }) => {
-    const language = $$queries.languages.getEntity(data.value);
-    return {
-      ...styles,
-      borderBottomRightRadius: 0,
-      borderTopRightRadius: 0,
-      ...(language?.color && language?.textColor
-        ? {
-            backgroundColor: language?.color ?? "white",
-            color: language?.textColor ?? "white",
-            borderRight: "rgba(0,0,0, .1) 1px solid",
-          }
-        : {}),
-    };
-  },
-  multiValueRemove: (styles, { data }) => {
-    const language = $$queries.languages.getEntity(data.value);
-    return {
-      ...styles,
-      borderBottomLeftRadius: 0,
-      borderTopLeftRadius: 0,
-      ...(language?.color && language?.textColor
-        ? {
-            backgroundColor: language?.color ?? "white",
-            color: language?.textColor ?? "white",
-          }
-        : {}),
-    };
-  },
-};
+interface IActivityCardProps {
+  activity: IActivity;
+  org: IOrg;
+}
 
 const ActivityCard: React.FC<IActivityCardProps> = ({ activity, org }) => (
   <div className="card">
--- react/src/react-select.tsx
+++ react/src/react-select.tsx
@@ -1,5 +1,9 @@
+import React from "react";
+
+import { components as ReactSelectComponents } from "react-select";
 import { ActionMeta, OptionProps, ValueType } from "react-select/src/types"; // tslint:disable-line no-submodule-imports
 import type { EntityStore, MultiActiveState, Store } from "@datorama/akita";
+import chroma from "chroma-js";
 
 export type ISelectOption = Pick<OptionProps, "label" | "value">;
 
@@ -23,4 +27,116 @@ export const onSelectChange = (setState: (_: any) => void) => (
   } else {
     setState([]);
   }
+};
+
+export const OrgOption = ({ children, data, ...props }) => {
+  const org = $$queries.orgs.getEntity(data?.value);
+  return (
+    <ReactSelectComponents.Option {...props}>
+      {children}{" "}
+      {org?.orgType && (
+        <>
+          <div
+            style={{
+              display: "inline-flex",
+              fontSize: ".75rem",
+              lineHeight: 1.5,
+              whiteSpace: "nowrap",
+              color: "#4a4a4a",
+              backgroundColor: "#f5f5f5",
+              padding: ".3rem .5rem",
+              borderRadius: ".3rem",
+            }}
+          >
+            {org.orgType}
+          </div>
+
+          {org.languages.map((languageId) => {
+            const language = $$queries.languages.getEntity(languageId);
+            if (!language) {
+              return null;
+            }
+            return (
+              <div
+                style={{
+                  display: "inline-flex",
+                  fontSize: ".75rem",
+                  lineHeight: 1.5,
+                  whiteSpace: "nowrap",
+                  color: language?.color
+                    ? chroma(language?.color).get("lab.l") > 70
+                      ? "black"
+                      : "white"
+                    : "#4a4a4a",
+                  backgroundColor: language?.color ?? "#f5f5f5",
+                  padding: ".3rem .5rem",
+                  marginLeft: ".5rem",
+                  borderRadius: ".3rem",
+                }}
+              >
+                {languageId}
+              </div>
+            );
+          })}
+        </>
+      )}
+    </ReactSelectComponents.Option>
+  );
+};
+
+export const languagesStyles = {
+  option: (styles, { data }) => {
+    const language = $$queries.languages.getEntity(data.value);
+    if (!language?.color && !language?.textColor) {
+      return styles;
+    }
+    const backgroundColor = chroma(language.color).alpha(0.5).css();
+    return {
+      ...styles,
+      backgroundColor,
+      color: chroma(backgroundColor).get("lab.l") > 80 ? "black" : "white",
+      "&:hover": {
+        backgroundColor: "#DEEBFF",
+      },
+    };
+  },
+
+  multiValue: (styles, { data }) => {
+    const language = $$queries.languages.getEntity(data.value);
+    // const color = chroma(data.color);
+    return {
+      ...styles,
+      // backgroundColor: color.alpha(0.1).css(),
+      backgroundColor: language.color,
+    };
+  },
+  multiValueLabel: (styles, { data }) => {
+    const language = $$queries.languages.getEntity(data.value);
+    return {
+      ...styles,
+      borderBottomRightRadius: 0,
+      borderTopRightRadius: 0,
+      ...(language?.color && language?.textColor
+        ? {
+            backgroundColor: language?.color ?? "white",
+            color: language?.textColor ?? "white",
+            borderRight: "rgba(0,0,0, .1) 1px solid",
+          }
+        : {}),
+    };
+  },
+  multiValueRemove: (styles, { data }) => {
+    const language = $$queries.languages.getEntity(data.value);
+    return {
+      ...styles,
+      borderBottomLeftRadius: 0,
+      borderTopLeftRadius: 0,
+      ...(language?.color && language?.textColor
+        ? {
+            backgroundColor: language?.color ?? "white",
+            color: language?.textColor ?? "white",
+          }
+        : {}),
+    };
+  },
 };
