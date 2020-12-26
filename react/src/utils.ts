6d6ea1d4c06c6120bafac1c3f585222ce4c80b44
Tony Narlock <tony@git-pull.com>
2020-12-25 21:06:27 -0600

    react/utils: Update typings

--- react/src/utils.ts
+++ react/src/utils.ts
@@ -1,10 +1,11 @@
 import React from "react";
 
 export const useAsyncEffect = (
-  effect: () => Promise<void>,
-  dependencies?: any[]
-) =>
+  // effect: () => Promise<void>,
+  effect: React.EffectCallback,
+  dependencies?: unknown[]
+): void =>
   React.useEffect(() => {
     effect();
-    return () => {};
+    return () => void 0;
   }, dependencies);
