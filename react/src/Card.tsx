155bf8a7efbfb4808b26c5520faccd1237fc7a53
Tony Narlock <tony@git-pull.com>
2020-12-28 14:14:19 -0600

    react: ActivityInfo

--- react/src/Card.tsx
+++ react/src/Card.tsx
@@ -82,18 +82,49 @@ export const OrgTypeTag: React.FC<
   );
 };
 
+export const PatchInfoBox: React.FC<
+  React.ComponentProps<typeof PatchInfoBox>
+> = ({ activity, org }) => {
+  return (
+    <>
+      {activity?.qaUrl && activity?.qaUrl !== "" && (
+        <a
+          href={activity.qaUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="activity-title"
+        >
+          PR
+        </a>
+      )}
+    </>
+  );
+};
+
+export const ActivityInfoBox: React.FC<
+  React.ComponentProps<typeof ActivityCard>
+> = ({ activity, org }) => {
+  return (
+    <div style={{ paddingLeft: "1rem", flexGrow: 1 }}>
+      {["Patch"].includes(activity.activityType) && (
+        <PatchInfoBox activity={activity} org={org} />
+      )}
+    </div>
+  );
+};
+
 export const ActivityCard: React.FC<IActivityCardProps> = ({
   activity,
   org,
 }) => (
   <div className="card">
-    <a
-      href={org && org.url ? org.url : "#"}
-      title={activity.title}
-      className="title"
-    >
-      {activity.title}
-    </a>
+    <div style={{ fontWeight: 600 }}>
+      <a href={org && org.url ? org.url : "#"} title={activity.title}>
+        {activity.title}
+      </a>
+    </div>
+    <ActivityInfoBox activity={activity} org={org} />
+
     {org?.languages?.map((languageName) => (
       <LanguageTag languageName={languageName} key={languageName} />
     ))}
