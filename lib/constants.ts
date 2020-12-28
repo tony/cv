9ced63b99b841920d2626dd5a4bba20ffba92ceb
Tony Narlock <tony@git-pull.com>
2020-12-28 13:13:18 -0600

    lib: Add regexes for activity typos, docs, etc.

--- lib/constants.ts
+++ lib/constants.ts
@@ -232,3 +232,8 @@ export const OrgTypeColors: {
 };
 
 export const LANGUAGE_FALLBACK_COLOR = "#f6f8fa";
+
+// via v1: https://github.com/tony/cv/blob/v1/lib/selectors.js
+export const reActivityTypoFix = /(typo|Typo|spelling|Spelling|note|Note|correct|Correct|Fix type|Fix URL|print statement|Remove duplicate)/;
+export const reActivityDocImprovement = /(doc|Doc|license|LICENSE|README|readme|link|Link|\.md|instructions|Instructions|guidelines|pypi badge|AUTHORS|License|changelog|label|copyright|add cookiecutter|issue template|awesome-|front-end frameworks|Examples for issue)|to other tools|sphinx|RelayHooks-Introduction|Changelog|changelog|CHANGES/;
+export const reActivityCodeStyleTweak = /(indent|Indent|whitespace|spacing|lint|Lint|sort|Sort|jshint|PEP|pep8|tabs|Tabs|Ignore|ignore|__about__|import|tweak|Tweak|hash|modernize|Add.*module|trivial|travis|Travis|dependency|MANIFEST.in|Pythonic|pythonic|exportable|empty line|Typing|typing|Typings|typings)/;
---
+++ lib/search/utils.ts
@@ -0,0 +1,16 @@
+import type { IActivity } from "../types";
+import {
+  reActivityTypoFix,
+  reActivityDocImprovement,
+  reActivityCodeStyleTweak,
+} from "../constants";
+
+// via v1: https://github.com/tony/cv/blob/v1/lib/selectors.js
+export const isActivityTypoFix = (activity: IActivity): boolean =>
+  !!activity.title.match(reActivityTypoFix);
+export const isActivityDocImprovement = (activity: IActivity): boolean =>
+  !!activity.title.match(reActivityDocImprovement);
+export const isActivityCodeStyleTweak = (activity: IActivity): boolean =>
+  !!activity.title.match(reActivityCodeStyleTweak);
+export const isActivityMerged = (activity: IActivity): boolean =>
+  activity?.acceptedDate ? true : false;
