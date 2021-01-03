adadd4a745c6b738ceef42d7130086da4839afee
Tony Narlock <tony@git-pull.com>
2021-01-02 19:48:50 -0600

    react: Initial integration of react-vis

---
+++ packages/chart-react-vis/src/charts.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import {
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  VerticalGridLines,
+  LineSeries,
+  LineSeriesPoint,
+  RadialChart,
+  makeVisFlexible,
+  FlexibleXYPlot,
+} from "react-vis";
+
+import type { Observable, Subscription } from "rxjs";
+
+import { visChartQuery as query } from "./hub";
+import { DonutChartProps, LineChartProps } from "./query";
+import equal from "fast-deep-equal";
+
+import "react-vis/dist/style.css";
+import "./chart-react-vis.scss";
+
+export const FlexRadialChart = makeVisFlexible(RadialChart);
+
+// Todo consolidate this into common code somewhere
+export function onEmit<T>(
+  source$: Observable<T>,
+  nextFn: (value: T) => void
+): Subscription {
+  return source$.subscribe(nextFn, console.error);
+}
+
+// Same as onEmit
+export const useAsyncEffect = (
+  effect: React.EffectCallback | (() => Promise<void>),
+  dependencies?: unknown[]
+): void =>
+  React.useEffect(() => {
+    effect();
+    return () => void 0;
+  }, dependencies);
+
+export const LanguagePieChart: React.FC<Partial<DonutChartProps>> = (props) => {
+  const [chartData, setChartData] = React.useState<DonutChartProps>();
+  const languageChartRef = React.useRef<typeof FlexRadialChart>(null);
+
+  useAsyncEffect(async () => {
+    if (!chartData) {
+      setChartData((await query.getDonutChart()) as DonutChartProps);
+    }
+    return void 0;
+  });
+
+  React.useEffect(() => void 0, [chartData]);
+  React.useEffect(() => {
+    const subscriptions: Subscription[] = [
+      onEmit<DonutChartProps>(query.subDonutChart$(), (newChart) => {
+        const isChanged = !equal(newChart, chartData);
+        console.log("pie published", newChart, chartData, { isChanged });
+
+        if (isChanged) {
+          setChartData(newChart);
+        }
+      }),
+    ];
+
+    return () => {
+      subscriptions.map((it) => it.unsubscribe());
+    };
+  }, []);
+
+  if (!chartData) {
+    return null;
+  }
+
+  return <FlexRadialChart ref={languageChartRef} {...chartData} {...props} />;
+};
+
+export const ActivityLineChart: React.FC<Partial<LineChartProps>> = (props) => {
+  const [chartData, setChartData] = React.useState<LineChartProps>();
+  const lineChartRef = React.useRef<FlexibleXYPlot>(null);
+
+  useAsyncEffect(async () => {
+    if (!chartData) {
+      setChartData((await query.getLineChart()) as LineChartProps);
+    }
+    return void 0;
+  });
+
+  React.useEffect(() => void 0, [chartData]);
+  React.useEffect(() => {
+    const subscriptions: Subscription[] = [
+      onEmit<LineChartProps>(query.subLineChart$(), (newChart) => {
+        const isChanged = !equal(newChart, chartData);
+        console.log("pie published", newChart, chartData, { isChanged });
+
+        if (isChanged) {
+          setChartData(newChart);
+        }
+      }),
+    ];
+
+    return () => {
+      subscriptions.map((it) => it.unsubscribe());
+    };
+  }, []);
+
+  if (!chartData) {
+    return null;
+  }
+
+  return (
+    <FlexibleXYPlot xType="ordinal" ref={lineChartRef} {...props}>
+      <LineSeries animation {...chartData} />
+      <HorizontalGridLines />
+      <VerticalGridLines />
+      <XAxis />
+      <YAxis />
+    </FlexibleXYPlot>
+  );
+};
---
+++ packages/chart-react-vis/src/hub.ts
@@ -0,0 +1,20 @@
+import {
+  activitiesQuery,
+  activityTypesQuery,
+  cvStore,
+  query,
+  orgsQuery,
+  orgTypesQuery,
+  languagesQuery,
+} from "@tony/cv-lib/hub";
+import { VisChartQuery } from "./query";
+
+export const visChartQuery = new VisChartQuery(
+  cvStore,
+  query,
+  activitiesQuery,
+  activityTypesQuery,
+  languagesQuery,
+  orgsQuery,
+  orgTypesQuery
+);
