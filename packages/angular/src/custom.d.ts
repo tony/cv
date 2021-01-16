196ebda44ba1cdfd419e5f07e3f434cfa18b9b40
Tony Narlock <tony@git-pull.com>
2021-01-16 07:16:22 -0600

    angular: Add cv-nav

---
+++ packages/angular/src/custom.d.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.html" {
+  const content: any;
+  export default content;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "cv-nav": any;
+  }
+}
