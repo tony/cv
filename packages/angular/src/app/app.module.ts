5c26091013072a568a79198117585e586986cd1f
Tony Narlock <tony@git-pull.com>
2021-01-16 09:49:36 -0600

    angular: Prevent component from be treeshaken

--- packages/angular/src/app/app.module.ts
+++ packages/angular/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import '@tony/cv-nav/components';
+import { MyNav } from '@tony/cv-nav/components';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [Title],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, MyNav],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
