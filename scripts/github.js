9b5b595daeb11aff16c8d677b5d9f08dcea8f936
Tony Narlock <tony@git-pull.com>
2021-01-23 07:42:13 -0600

    github: Format with prettier

--- scripts/github.js
+++ scripts/github.js
@@ -1,32 +1,39 @@
-const fs = require('fs');
-var path = require('path');
-var client = require('github-graphql-client');
-const util = require('util');
+const fs = require("fs");
+var path = require("path");
+var client = require("github-graphql-client");
+const util = require("util");
 const c = util.promisify(client);
-const moment = require('moment');
+const moment = require("moment");
 
-
-const gh_token = process.env.GITHUB_API_TOKEN || process.env.HOMEBREW_GITHUB_API_TOKEN;
+const gh_token =
+  process.env.GITHUB_API_TOKEN || process.env.HOMEBREW_GITHUB_API_TOKEN;
 
 if (!gh_token) {
   console.error(
-    'GITHUB_API_TOKEN or HOMEBREW_GITHUB_API_TOKEN must be set in ENV'
+    "GITHUB_API_TOKEN or HOMEBREW_GITHUB_API_TOKEN must be set in ENV"
   );
   process.exit(0);
 }
 
 const config = {
   exclude_own_repo: true,
-  gh_user: 'tony',
-  exclude_users: ['cihai', 'git-pull', 'vcs-python', 'tmux-python', 'nick-ma', 'emre', 'joseph-flinn'],
-  output_dir: 'data/scraped',
+  gh_user: "tony",
+  exclude_users: [
+    "cihai",
+    "git-pull",
+    "vcs-python",
+    "tmux-python",
+    "nick-ma",
+    "emre",
+    "joseph-flinn",
+  ],
+  output_dir: "data/scraped",
   ignore_private_repos: true,
-}
+};
 
-if (!fs.existsSync(config.output_dir)){
-    fs.mkdirSync(config.output_dir);
+if (!fs.existsSync(config.output_dir)) {
+  fs.mkdirSync(config.output_dir);
 }
-
 
 const testQuery = `
   query {
@@ -34,19 +41,20 @@ const testQuery = `
       login
     }
   }
-`
+`;
 
 // note, this requires .replace of:
 // 1. {{user_params}} -> 'login: "yourusername"'
 // 2. {{pull_requests_params}} -> 'first 100'
 const prQuery = fs.readFileSync(
-  path.join(__dirname, './gh_pullrequests.graphql'), 'utf8'
+  path.join(__dirname, "./gh_pullrequests.graphql"),
+  "utf8"
 );
 
 async function ghQuery(query) {
   const q = await c({
     token: gh_token,
-    query: query
+    query: query,
   });
   return q;
 }
@@ -55,28 +63,36 @@ async function ghQuery(query) {
 //   console.log(data);
 // });
 
-const userPrQuery = prQuery.replace('{{user_params}}', `login: "${config.gh_user}"`)
+const userPrQuery = prQuery.replace(
+  "{{user_params}}",
+  `login: "${config.gh_user}"`
+);
 const initialPrQuery = userPrQuery.replace(
-  '{{pull_requests_params}}', 'first: 100'
+  "{{pull_requests_params}}",
+  "first: 100"
 );
 
-const makePRQuery = async (query) => {  // wraps ghQuery
+const makePRQuery = async (query) => {
+  // wraps ghQuery
   const res = await ghQuery(query);
-  const data = {  // create a compact response (non-standard)
+  const data = {
+    // create a compact response (non-standard)
     ...{ pullRequests: res.data.user.pullRequests.edges },
     ...res.data.user.pullRequests.pageInfo,
   };
   if (data.hasNextPage) {
     data.nextQuery = userPrQuery.replace(
-      '{{pull_requests_params}}', `first: 100, after: "${data.endCursor}"`
+      "{{pull_requests_params}}",
+      `first: 100, after: "${data.endCursor}"`
     );
   }
   return data;
-}
+};
 
 let issues = [];
 
-const recursePRQuery = async (query) => {  // on first invocation, add initial query
+const recursePRQuery = async (query) => {
+  // on first invocation, add initial query
   const res = await makePRQuery(query);
 
   issues = issues.concat(res.pullRequests);
@@ -85,78 +101,86 @@ const recursePRQuery = async (query) => {  // on first invocation, add initial q
   } else {
     return issues;
   }
-}
+};
 
-
-recursePRQuery(initialPrQuery).then(prs => {
-  if (config.exclude_own_repo) {
-    prs = prs.filter(pr => !pr.node.url.includes(`https://github.com/${config.gh_user}/`));
-  }
-  if (config.exclude_users) {
-    for (let user of config.exclude_users) {
-      prs = prs.filter(pr => !pr.node.url.includes(`https://github.com/${user}/`));
+recursePRQuery(initialPrQuery)
+  .then((prs) => {
+    if (config.exclude_own_repo) {
+      prs = prs.filter(
+        (pr) => !pr.node.url.includes(`https://github.com/${config.gh_user}/`)
+      );
     }
-  }
-
-  if (config.ignore_private_repos) {
-    prs = prs.filter(pr => !pr.node.repository.isPrivate);
-  }
-
-  prs = prs.map(pr => pr.node);  // zoom in on "node"
-
-
-  let projects = prs.map(pr => pr.repository);
-
-  // join languages
-  projects = projects.map(p => {
-    if (p.languages.edges.length) {
-      p.languages = [p.languages.edges[0].node.name];
-    } else {
-      p.languages = undefined;
+    if (config.exclude_users) {
+      for (let user of config.exclude_users) {
+        prs = prs.filter(
+          (pr) => !pr.node.url.includes(`https://github.com/${user}/`)
+        );
+      }
     }
-    return p;
-  });
-  // console.log(projects);
-  // console.log(projects.length);
 
-  // only have unique stuff
-  // projects = [ ... new Set(projects) ];
-  projects = projects.filter((project, index, self) => self.findIndex(p => p.name === project.name) === index)
-  // console.log(projects);
-  // console.log(projects.length);
-
-  let id = 0;
-  const projects_final = projects.map(p => {
-    return {
-      id: id++,
-      type: 'Open Source',
-      name: p.name,
-      url: p.homepageUrl,
-      repo_url: p.url,
-      languages: p.languages
+    if (config.ignore_private_repos) {
+      prs = prs.filter((pr) => !pr.node.repository.isPrivate);
     }
-  });
 
-  let data = JSON.stringify(projects_final, null, '  ');
-  fs.writeFileSync(`${config.output_dir}/gh_actors.json`, data);
+    prs = prs.map((pr) => pr.node); // zoom in on "node"
 
-  // we do this at the bottom because we want to associate the
-  // project_final 'id' attribute with project in pr's
+    let projects = prs.map((pr) => pr.repository);
 
-  id = 0;
-  const pullRequests_final = prs.map(pr => {
-    return {
-      id: id++,
-      component_name: 'Patch',
-      qa_url: pr.url,
-      diff_url: pr.url + '.diff',
-      created_date: moment(pr.createdAt).format('YYYY-MM-DD'),
-      accepted_date: pr.mergedAt ? moment(pr.mergedAt).format('YYYY-MM-DD') : null,
-      title: pr.title,
-      actor: projects_final.find(p => pr.repository.name == p.name).id,
-    }
-  });
-  data = JSON.stringify(pullRequests_final, null, '  ');
-  fs.writeFileSync(`${config.output_dir}/gh_activities.json`, data);
+    // join languages
+    projects = projects.map((p) => {
+      if (p.languages.edges.length) {
+        p.languages = [p.languages.edges[0].node.name];
+      } else {
+        p.languages = undefined;
+      }
+      return p;
+    });
+    // console.log(projects);
+    // console.log(projects.length);
 
-}).catch(err => console.error(err));
+    // only have unique stuff
+    // projects = [ ... new Set(projects) ];
+    projects = projects.filter(
+      (project, index, self) =>
+        self.findIndex((p) => p.name === project.name) === index
+    );
+    // console.log(projects);
+    // console.log(projects.length);
+
+    let id = 0;
+    const projects_final = projects.map((p) => {
+      return {
+        id: id++,
+        type: "Open Source",
+        name: p.name,
+        url: p.homepageUrl,
+        repo_url: p.url,
+        languages: p.languages,
+      };
+    });
+
+    let data = JSON.stringify(projects_final, null, "  ");
+    fs.writeFileSync(`${config.output_dir}/gh_actors.json`, data);
+
+    // we do this at the bottom because we want to associate the
+    // project_final 'id' attribute with project in pr's
+
+    id = 0;
+    const pullRequests_final = prs.map((pr) => {
+      return {
+        id: id++,
+        component_name: "Patch",
+        qa_url: pr.url,
+        diff_url: pr.url + ".diff",
+        created_date: moment(pr.createdAt).format("YYYY-MM-DD"),
+        accepted_date: pr.mergedAt
+          ? moment(pr.mergedAt).format("YYYY-MM-DD")
+          : null,
+        title: pr.title,
+        actor: projects_final.find((p) => pr.repository.name == p.name).id,
+      };
+    });
+    data = JSON.stringify(pullRequests_final, null, "  ");
+    fs.writeFileSync(`${config.output_dir}/gh_activities.json`, data);
+  })
+  .catch((err) => console.error(err));
