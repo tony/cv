44c647da845adbcfb18b2dbcd53a60743d97c2b9
Tony Narlock <tony@git-pull.com>
2021-01-23 10:41:06 -0600

    react: Add until date

--- packages/react/src/Card.tsx
+++ packages/react/src/Card.tsx
@@ -69,13 +69,12 @@ export const ActivityInfo: React.FC<
   );
 };
 
-const DateText = ({ date }: { date: string }) => (
+const DateText: React.FC<
+  { date: string } & React.HTMLProps<HTMLSpanElement>
+> = ({ date, ...rest }) => (
   <>
     {date && (
-      <span
-        title={format(new Date(date), "MMMM do, yyyy")}
-        style={{ paddingLeft: "0.25rem" }}
-      >
+      <span title={format(new Date(date), "MMMM do, yyyy")} {...rest}>
         {formatDistance(new Date(date), new Date())} ago
       </span>
     )}
@@ -113,7 +112,16 @@ export const ActivityCard: React.FC<IActivityCardProps> = ({
             startedAt={activity.startedAt}
             endedAt={activity.endedAt}
           />
-          <DateText date={activity.createdAt} />
+          <DateText
+            date={activity.createdAt}
+            style={{ paddingLeft: "0.25rem" }}
+          />
+          {activity.endedAt && (
+            <>
+              {" "}
+              until <DateText date={activity.endedAt} />
+            </>
+          )}
         </span>
       </div>
       <div style={{ paddingTop: "0.25rem", fontSize: "1rem" }}>
