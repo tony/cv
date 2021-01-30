63041b823dbd2898eabc80f90d44f9196072b5da
Tony Narlock <tony@git-pull.com>
2021-01-30 15:24:03 -0600

    nav: Highlight current area, add link to vue v1

--- packages/nav/components/nav.ts
+++ packages/nav/components/nav.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, customElement, css, unsafeCSS } from "lit-element";
+import { LitElement, html, customElement, css, property, unsafeCSS } from "lit-element";
 import type { CSSResult, TemplateResult } from "lit-element";
 
 import style from "!raw-loader!sass-loader!./nav.scss";
@@ -7,8 +7,52 @@ import reactSvg from "@tony/cv-data/img/icons/react.svg";
 import angularSvg from "@tony/cv-data/img/icons/angular.svg";
 import vueSvg from "@tony/cv-data/img/icons/vue.svg";
 
+enum Framework {
+  Angular = "Angular",
+  React = "React",
+  Vue = "Vue",
+}
+
+enum CVVersion {
+  V1 = "V1",
+  V2 = "V2",
+}
+
+const getActiveFramework = (): Framework | null => {
+  const url = window.location.toString();
+  switch (url) {
+    case url.match(/angular|:4200/)?.input: {
+      return Framework.Angular;
+    }
+    case url.match(/react|:3099/)?.input: {
+      return Framework.React;
+    }
+    case url.match(/vue|:3093/)?.input: {
+      return Framework.Vue;
+    }
+    default:
+      return null;
+  }
+};
+// @ts-ignore
+window.getActiveFramework = getActiveFramework;
+
+const getCVVersion = (): CVVersion | null => {
+  const url = window.location.toString();
+  switch (url) {
+    case url.match(/v2/)?.input: {
+      return CVVersion.V2;
+    }
+    default:
+      return CVVersion.V2;
+  }
+};
+
 @customElement("cv-nav")
 export class CVNav extends LitElement {
+  @property() cv_version = getCVVersion();
+  @property() framework = getActiveFramework();
+
   static get styles(): CSSResult[] {
     return [
       css`
@@ -28,7 +72,7 @@ export class CVNav extends LitElement {
           </b>
         </div>
         <ul>
-          <li>
+          <li class="${this.framework == Framework.React ? "active" : ""}">
             <a href="https://cv-react-v2.git-pull.com/"><img src="${reactSvg}" width="18" /> React</a>
             <ul class="submenu">
               <li class="section-title">
@@ -37,16 +81,37 @@ export class CVNav extends LitElement {
               <li>
                 <a href="https://cv-react-v1.git-pull.com/">v1 (2018)</a>
               </li>
-              <li class="active">
-                <a href="https://cv-react-v2.git-pull.com/">v2 (2021) -&nbsp;<strong>You are here</strong></a>
+              <li class="${this.framework == Framework.React ? "active" : ""}">
+                <a href="https://cv-react-v2.git-pull.com/"
+                  >v2
+                  (2021)${this.framework == Framework.React
+                    ? html` -&nbsp;<strong>You are here</strong> `
+                    : ""}
+                </a>
               </li>
             </ul>
           </li>
-          <li>
+          <li class="${this.framework == Framework.Angular ? "active" : ""}">
             <a href="https://cv-angular-v2.git-pull.com/"> <img src="${angularSvg}" width="18" /> Angular </a>
           </li>
-          <li>
+          <li class="${this.framework == Framework.Vue ? "active" : ""}">
             <a href="https://cv-vue-v2.git-pull.com/"><img src="${vueSvg}" width="18" /> Vue</a>
+            <ul class="submenu">
+              <li class="section-title">
+                <strong>Versions</strong>
+              </li>
+              <li>
+                <a href="https://cv-vue-v1.git-pull.com/">v1 (2018)</a>
+              </li>
+              <li class="${this.framework == Framework.Vue ? "active" : ""}">
+                <a href="https://cv-vue-v2.git-pull.com/"
+                  >v2
+                  (2021)${this.framework == Framework.Vue
+                    ? html` -&nbsp;<strong>You are here</strong> `
+                    : ""}</a
+                >
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>
