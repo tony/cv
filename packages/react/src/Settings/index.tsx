58a395f2b8ff4814495c17522a04923c3a7a9992
Tony Narlock <tony@git-pull.com>
2021-02-20 13:40:57 -0600

    react: Split FilterDropdowns

--- packages/react/src/Settings/index.tsx
+++ packages/react/src/Settings/index.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import Select from "react-select";
 
-import {
-  activityTypesQuery,
-  cvService,
-  languagesQuery,
-  orgsQuery,
-} from "@tony/cv-lib/hub";
+import { cvService } from "@tony/cv-lib/hub";
 import type { Results as ReducerState } from "@tony/cv-lib/search/query";
 
 import "@tony/cv-ui-range-slider";
@@ -15,24 +9,10 @@ import type {
   CustomEventMap as RangeSliderEvents,
 } from "@tony/cv-ui-range-slider";
 
-import {
-  ActivityMultiValueLabel,
-  activityTypeStyles,
-  getSelectOptions,
-  languagesStyles,
-  LanguageOption,
-  ActivityTypeOption,
-  OrgOption,
-  onLanguageChange,
-  onOrgChange,
-  onActivityTypeChange,
-} from "../react-select";
-import type { ISelectOption } from "../react-select";
-
+import { FilterDropdowns } from "./FilterDropdowns";
 import { FilterToggles } from "./FilterToggles";
 
 export const Settings: React.FC<{ results: ReducerState }> = ({ results }) => {
-  const languageSelectRef = React.useRef<Select>(null);
   const histogramRangeSliderRef = React.useRef<RangeSlider>(null);
 
   const onHistogramChange = (e: RangeSliderEvents["change.one"]) => {
@@ -65,52 +45,7 @@ export const Settings: React.FC<{ results: ReducerState }> = ({ results }) => {
 
   return (
     <div id="settings">
-      <div className="dropdownRow">
-        <Select
-          options={getSelectOptions(
-            Object.values(languagesQuery?.getValue()?.entities ?? {}).map(
-              (lang) => lang.id as string
-            )
-          )}
-          ref={languageSelectRef}
-          isMulti
-          onChange={onLanguageChange}
-          className="react-select"
-          placeholder="Language"
-          styles={languagesStyles}
-          components={{ Option: LanguageOption }}
-        />
-        <Select
-          options={
-            activityTypesQuery.getAll().map((a) => ({
-              label: a.name,
-              value: a.id,
-            })) as ISelectOption
-          }
-          isMulti={true}
-          onChange={onActivityTypeChange}
-          className="react-select"
-          placeholder="Event type"
-          styles={activityTypeStyles}
-          components={{
-            Option: ActivityTypeOption,
-            MultiValueLabel: ActivityMultiValueLabel,
-          }}
-        />
-        <Select
-          options={
-            orgsQuery.getAll().map((org) => ({
-              label: org.name,
-              value: org.id?.toString() ?? org.id,
-            })) as ISelectOption
-          }
-          isMulti={true}
-          onChange={onOrgChange}
-          className="react-select"
-          placeholder="Topic"
-          components={{ Option: OrgOption }}
-        />
-      </div>
+      <FilterDropdowns />
       <range-slider
         id="year-range"
         ref={histogramRangeSliderRef}
