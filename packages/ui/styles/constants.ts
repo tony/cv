23db7cccefdc0476b06dd3056a3f40efef8b9f2c
Tony Narlock <tony@git-pull.com>
2021-02-22 18:39:22 -0600

    ui: Graphs: 400 -> 250

--- packages/ui/styles/constants.ts
+++ packages/ui/styles/constants.ts
@@ -1,13 +1,13 @@
-export const donutChartHeight = 400;
+export const donutChartHeight = 250;
 export const donutChartHeightString = `${donutChartHeight}`;
 export const donutChartHeightWithUnit = `${donutChartHeight}px`;
-export const donutChartWidth = 400;
+export const donutChartWidth = 250;
 export const donutChartWidthString = `${donutChartWidth}`;
 export const donutChartWidthWithUnit = `${donutChartWidth}px`;
 
-export const lineChartHeight = 400;
+export const lineChartHeight = 250;
 export const lineChartHeightString = `${lineChartHeight}`;
 export const lineChartHeightWithUnit = `${lineChartHeight}px`;
-export const lineChartWidth = 400;
+export const lineChartWidth = 250;
 export const lineChartWidthString = `${lineChartWidth}`;
 export const lineChartWidthWithUnit = `${lineChartWidth}px`;
