53de5ab3de138cae08e4e458f3a525a5c8c6cf6c
Tony Narlock <tony@git-pull.com>
2021-02-24 21:35:40 -0600

    angular: Fix cv-nav path

--- packages/angular/src/app/app.module.ts
+++ packages/angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CVNav } from '@tony/cv-nav';
+import { CVNav } from '@tony/cv-nav/nav';
 
 @NgModule({
   declarations: [AppComponent],
