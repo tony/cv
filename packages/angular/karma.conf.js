0336a585d1430446a898ce908495e181eb40bd71
Tony Narlock <tony@git-pull.com>
2021-11-27 10:39:03 -0600

    angular: Fix karma.conf.js json

--- packages/angular/karma.conf.js
+++ packages/angular/karma.conf.js
@@ -1,18 +1,18 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     autoWatch: true,
     basePath: '',
     browsers: ['Chrome'],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     colors: true,
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/angular'),
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
       reports: ['html', 'lcovonly', 'text-summary'],
     },
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -22,11 +22,11 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     port: 9876,
     reporters: ['progress', 'kjhtml'],
-    restartOnFileChange: true
+    restartOnFileChange: true,
     singleRun: false,
   });
 };
