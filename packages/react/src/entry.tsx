da97e46ec57d357db83ead78306bbdebdac9a17e
Tony Narlock <tony@git-pull.com>
2022-03-30 07:10:29 -0500

    react: ReactDOM.render -> ReactDOMClient.createRoot

--- packages/react/src/entry.tsx
+++ packages/react/src/entry.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOMClient from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+if (container) {
+  const root = ReactDOMClient.createRoot(container);
+  root.render(<App />);
+}
