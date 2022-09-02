bd3114cac2e67eb5cac847f6e95bbe3031c1b674
Tony Narlock <tony@git-pull.com>
2022-09-02 18:21:25 -0500

    vue(eslint): Update to vuejs/eslint-config-typescript

--- packages/vue/.eslintrc.js
+++ packages/vue/.eslintrc.js
@@ -1,18 +1,27 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended"],
+  extends: [
+    "plugin:vue/essential",
+    // "eslint:recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-typescript",
+    // "@vue/eslint-config-prettier",
+  ],
 
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 18,
     parser: "@typescript-eslint/parser",
     sourceType: "module",
     extraFileExtensions: [".vue"],
     project: "./tsconfig.json",
   },
-  plugins: ["vue", "@typescript-eslint"],
+  // plugins: ["vue", "@typescript-eslint"],
   rules: {},
 };
