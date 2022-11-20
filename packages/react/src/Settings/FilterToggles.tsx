691c8cce326ce83b020b85217976a97ba8056423
Tony Narlock <tony@git-pull.com>
2022-11-20 10:47:23 -0600

    react(filter toggles): Move to @atlaskit/toggle

--- packages/react/src/Settings/FilterToggles.tsx
+++ packages/react/src/Settings/FilterToggles.tsx
@@ -2,20 +2,15 @@ import React from "react";
 
 import { cvService } from "@tony/cv-lib/hub";
 
-import { Switcher } from "@tony/cv-ui-switch";
-import type { CustomEventMap as SwitcherEvents } from "@tony/cv-ui-switch";
+import Toggle from "@atlaskit/toggle";
+
+import "./FilterToggles.css";
 
 export const FilterToggles: React.FC = () => {
-  const onShowSpellingContributionsRef = React.useRef<Switcher>(null);
-  const onShowReleaseContributionsRef = React.useRef<Switcher>(null);
-  const onShowDocumentationContributionsRef = React.useRef<Switcher>(null);
-  const onShowCodeStyleContributionsRef = React.useRef<Switcher>(null);
-  const onShowUnmergedContributionsRef = React.useRef<Switcher>(null);
-
   const onShowReleaseContributionsChange = (
-    e: SwitcherEvents["change.one"]
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const checked = e?.detail?.checked;
+    const checked = e?.target?.checked;
     if (checked !== null) {
       cvService.setActivityFilters({
         showReleases: checked,
@@ -23,9 +18,9 @@ export const FilterToggles: React.FC = () => {
     }
   };
   const onShowSpellingContributionsChange = (
-    e: SwitcherEvents["change.one"]
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const checked = e?.detail?.checked;
+    const checked = e?.target?.checked;
     if (checked !== null) {
       cvService.setActivityFilters({
         showTypos: checked,
@@ -33,9 +28,9 @@ export const FilterToggles: React.FC = () => {
     }
   };
   const onShowDocumentationContributionsChange = (
-    e: SwitcherEvents["change.one"]
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const checked = e?.detail?.checked;
+    const checked = e?.target?.checked;
     if (checked !== null) {
       cvService.setActivityFilters({
         showDocImprovements: checked,
@@ -43,9 +38,9 @@ export const FilterToggles: React.FC = () => {
     }
   };
   const onShowCodeStyleContributionsChange = (
-    e: SwitcherEvents["change.one"]
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const checked = e?.detail?.checked;
+    const checked = e?.target?.checked;
     if (checked !== null) {
       cvService.setActivityFilters({
         showCodeStyleTweaks: checked,
@@ -53,9 +48,9 @@ export const FilterToggles: React.FC = () => {
     }
   };
   const onShowUnmergedContributionsChange = (
-    e: SwitcherEvents["change.one"]
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const checked = e?.detail?.checked;
+    const checked = e?.target?.checked;
     if (checked !== null) {
       cvService.setActivityFilters({
         showUnmerged: checked,
@@ -63,90 +58,33 @@ export const FilterToggles: React.FC = () => {
     }
   };
 
-  React.useLayoutEffect(() => {
-    const releaseSwitcher = onShowReleaseContributionsRef?.current;
-    const spellingSwitcher = onShowSpellingContributionsRef?.current;
-    const documentationSwitcher = onShowDocumentationContributionsRef?.current;
-    const codeStyleSwitcher = onShowCodeStyleContributionsRef?.current;
-    const unmergedSwitcher = onShowUnmergedContributionsRef?.current;
-    if (
-      !releaseSwitcher ||
-      !spellingSwitcher ||
-      !documentationSwitcher ||
-      !codeStyleSwitcher ||
-      !unmergedSwitcher
-    ) {
-      return;
-    }
-
-    if (spellingSwitcher.addEventListener) {
-      releaseSwitcher.addEventListener(
-        "change.one",
-        onShowReleaseContributionsChange
-      );
-      spellingSwitcher.addEventListener(
-        "change.one",
-        onShowSpellingContributionsChange
-      );
-      documentationSwitcher.addEventListener(
-        "change.one",
-        onShowDocumentationContributionsChange
-      );
-      codeStyleSwitcher.addEventListener(
-        "change.one",
-        onShowCodeStyleContributionsChange
-      );
-      unmergedSwitcher.addEventListener(
-        "change.one",
-        onShowUnmergedContributionsChange
-      );
-    }
-    return () => {
-      releaseSwitcher.removeEventListener(
-        "change.one",
-        onShowReleaseContributionsChange
-      );
-      spellingSwitcher.removeEventListener(
-        "change.one",
-        onShowSpellingContributionsChange
-      );
-      documentationSwitcher.removeEventListener(
-        "change.one",
-        onShowDocumentationContributionsChange
-      );
-      codeStyleSwitcher.removeEventListener(
-        "change.one",
-        onShowCodeStyleContributionsChange
-      );
-      unmergedSwitcher.removeEventListener(
-        "change.one",
-        onShowUnmergedContributionsChange
-      );
-    };
-  });
-
   return (
     <div className="toggles">
-      <Switcher
+      <Toggle
         id="show-documentation"
-        ref={onShowDocumentationContributionsRef}
+        onChange={onShowDocumentationContributionsChange}
+      />
+      <label htmlFor="show-documentation">Docs</label>
+
+      <Toggle id="show-release" onChange={onShowReleaseContributionsChange} />
+      <label
+        htmlFor="show-release"
+        title="Continuous integration, build and releases"
       >
-        Docs
-      </Switcher>
-      <Switcher id="show-release" ref={onShowReleaseContributionsRef}>
-        <span title="Continuous integration, build and releases">
-          Build / Release / CI
-        </span>
-      </Switcher>
-      <Switcher id="show-code-style" ref={onShowCodeStyleContributionsRef}>
-        Code Style
-      </Switcher>
-      <Switcher id="show-spelling" ref={onShowSpellingContributionsRef}>
-        Typos
-      </Switcher>
-      <Switcher id="show-unmerged" ref={onShowUnmergedContributionsRef}>
-        Unmerged
-      </Switcher>
+        Build / Release /CI
+      </label>
+
+      <Toggle
+        id="show-code-style"
+        onChange={onShowCodeStyleContributionsChange}
+      />
+      <label htmlFor="show-code-style">Code Style</label>
+
+      <Toggle id="show-spelling" onChange={onShowSpellingContributionsChange} />
+      <label htmlFor="show-spelling">Typos</label>
+
+      <Toggle id="show-unmerged" onChange={onShowUnmergedContributionsChange} />
+      <label htmlFor="show-unmerged">Unmerged</label>
     </div>
   );
 };
