45a8ac6eb03a1fe271fac784d59e4ef0d6037479
Tony Narlock <tony@git-pull.com>
2023-07-28 19:26:41 -0500

    vite,react: Add IBM Plex Sans, Inconsolata

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import type { ConfigEnv, UserConfig } from "vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 export default defineConfig(({ command }: ConfigEnv): UserConfig => {
   const config: UserConfig = {
@@ -9,6 +10,7 @@ export default defineConfig(({ command }: ConfigEnv): UserConfig => {
         // Only .tsx files
         include: "**/*.tsx",
       }),
+      webfontDownload(),
     ],
 
     build: {
