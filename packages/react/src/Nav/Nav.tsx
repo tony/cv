9a8a694b265fa2c168889f10f92654e95e05982c
Tony Narlock <tony@git-pull.com>
2023-08-05 13:43:56 -0500

    ux: Add container boundary (including to navbar)

--- packages/react/src/Nav/Nav.tsx
+++ packages/react/src/Nav/Nav.tsx
@@ -6,14 +6,16 @@ import { ColorSchemeToggle } from "./ColorScheme";
 
 export const Nav: React.FC = () => (
   <nav id="top-nav">
-    <div className="site-info" title="Tony Narlock's CV">
-      <div className="site-info--logo">
-        <div className="site-info--logo--img">&nbsp;</div>
-      </div>
-      <div className="site-info--site-name">Tony Narlock&apos;s CV</div>
-      <div className="site-info--filler">&nbsp;</div>
-      <div className="site-info--color-scheme-toggle--container">
-        <ColorSchemeToggle />
+    <div className="top-nav--container">
+      <div className="site-info" title="Tony Narlock's CV">
+        <div className="site-info--logo">
+          <div className="site-info--logo--img">&nbsp;</div>
+        </div>
+        <div className="site-info--site-name">Tony Narlock&apos;s CV</div>
+        <div className="site-info--filler">&nbsp;</div>
+        <div className="site-info--color-scheme-toggle--container">
+          <ColorSchemeToggle />
+        </div>
       </div>
     </div>
   </nav>
