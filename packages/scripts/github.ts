118b72d5c2b02b2519cac23e6614b312cb808d63
Tony Narlock <tony@git-pull.com>
2023-11-18 06:34:36 -0600

    GitHub Script: Use octokit package
    Which has integration with retries and throttling.

--- packages/scripts/github.ts
+++ packages/scripts/github.ts
@@ -1,8 +1,7 @@
 import fs from "fs";
 
-import { Octokit } from "@octokit/core";
-import { paginateGraphql } from "@octokit/plugin-paginate-graphql";
 import moment from "moment";
+import { Octokit } from "octokit";
 
 const ghToken =
   process.env.GITHUB_API_TOKEN || process.env.HOMEBREW_GITHUB_API_TOKEN;
@@ -41,8 +40,7 @@ if (!fs.existsSync(config.output_dir)) {
   fs.mkdirSync(config.output_dir);
 }
 
-const MyOctokit = Octokit.plugin(paginateGraphql);
-const octokit = new MyOctokit({ auth: ghToken });
+const octokit = new Octokit({ auth: ghToken });
 
 let issues = [];
 
