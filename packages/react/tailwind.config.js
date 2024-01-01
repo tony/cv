fbf6398e451a67eda4a1c6bd18d6d5f01422b2fd
Tony Narlock <tony@git-pull.com>
2024-01-01 07:35:16 -0600

    Tailwind CSS configuration updates

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -2,7 +2,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: {
+    relative: true,
+    files: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  },
   theme: {
     extend: {
       fontFamily: {
@@ -10,5 +13,9 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
 };
