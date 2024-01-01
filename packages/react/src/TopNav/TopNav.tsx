d6bcd680a05eb7a3ad22a984510f88f2109d8d91
Tony Narlock <tony@git-pull.com>
2024-01-01 08:05:24 -0600

    TopNav: Convert colors to tailwind

--- packages/react/src/TopNav/TopNav.tsx
+++ packages/react/src/TopNav/TopNav.tsx
@@ -5,7 +5,10 @@ import "./TopNav.css";
 import { ColorSchemeToggle } from "./ColorScheme";
 
 export const TopNav: React.FC = () => (
-  <nav id="top-nav">
+  <nav
+    id="top-nav"
+    className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-blue-800/90 text-white supports-backdrop-blur:bg-white/60 dark:bg-transparent"
+  >
     <div className="top-nav--container">
       <div className="site-info" title="Tony Narlock's CV">
         <div className="site-info--logo">
