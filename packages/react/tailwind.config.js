609624bb13ee1f95297bbd7322a8045b1f9b47f7
Tony Narlock <tony@git-pull.com>
2024-01-01 07:17:41 -0600

    tailwind config: Add IBM Plex Sans font

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans", "Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
