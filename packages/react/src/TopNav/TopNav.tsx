9e325585f3e2bf3e339e5e6b9064b362d204aca2
Tony Narlock <tony@git-pull.com>
2024-01-01 09:52:47 -0600

    TopNav: Use colors in dark mode

--- packages/react/src/TopNav/TopNav.tsx
+++ packages/react/src/TopNav/TopNav.tsx
@@ -7,7 +7,7 @@ import { ColorSchemeToggle } from "./ColorScheme";
 export const TopNav: React.FC = () => (
   <nav
     id="top-nav"
-    className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-blue-800/90 text-white supports-backdrop-blur:bg-white/60 dark:bg-transparent"
+    className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-blue-800/90 text-white supports-backdrop-blur:bg-white/60 dark:bg-black-pearl-700/20"
   >
     <div className="top-nav--container">
       <div className="site-info" title="Tony Narlock's CV">
